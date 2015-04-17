be72bd8c4574444d57b28a93b9b430f4cef7ec05
Vocatus Gate <vocatus.gate@gmail.com>
2015-04-17 14:43:38 -0700

    Create reset_registry_permissions.bat

---
+++ resources/stage_4_repair/reset_registry_and_file_permissions/reset_registry_permissions.bat
@@ -0,0 +1,38 @@
+:: Purpose:       Restores full rights to Administrator and SYSTEM accounts on everything in specified registry hives
+:: Requirements:  subinacl in the system path or system32 folder
+:: Author:        reddit.com/user/vocatus ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
+::                1.0.0-TRON / Modifications for Tron. Remove logging, CUR_DATE, and various other unnecessary code
+::                1.0.0        Initial write
+SETLOCAL
+
+
+:::::::::::::::
+:: VARIABLES ::
+:::::::::::::::
+:: no user-set variables for this script
+
+
+::::::::::
+:: Prep :: -- Don't change anything in this section
+::::::::::
+@echo off
+SETLOCAL
+set SCRIPT_VERSION=1.0.0-TRON
+set SCRIPT_UPDATED=2015-04-16
+
+
+:::::::::::::
+:: EXECUTE ::
+:::::::::::::
+subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_HKLM.log /subkeyreg HKEY_LOCAL_MACHINE /grant=administrators=f
+subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_HKCU.log /subkeyreg HKEY_CURRENT_USER /grant=administrators=f
+subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_HKCR.log /subkeyreg HKEY_CLASSES_ROOT /grant=administrators=f
+subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_HKLM1.log /subkeyreg HKEY_LOCAL_MACHINE /grant=system=f
+subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_HKCU1.log /subkeyreg HKEY_CURRENT_USER /grant=system=f
+subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_HKCR1.log /subkeyreg HKEY_CLASSES_ROOT /grant=system=f
+
+:: Compile the logs
+type subinacl_reg_reset_HKLM1.log >> subinacl_reg_reset_HKLM.log
+type subinacl_reg_reset_HKCU1.log >> subinacl_reg_reset_HKCU.log
+type subinacl_reg_reset_HKCR1.log >> subinacl_reg_reset_HKCR.log
+del /f /q *1.log >NUL 2>&1
