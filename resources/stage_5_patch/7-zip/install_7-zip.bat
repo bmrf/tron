c6331fb9ce4b8a33b3ad6daafa282d4af7e2c0ca
Spexdi <sumfungai42@gmail.com>
2015-10-13 14:48:35 -0700

    Update version, header information

--- resources/stage_5_patch/7-zip/install_7-zip.bat
+++ resources/stage_5_patch/7-zip/install_7-zip.bat
@@ -1,6 +1,7 @@
 :: Purpose:       Installs a package
 :: Requirements:  Run this script with a network admin account
 :: Author:        reddit.com/user/vocatus ( vocatus.gate@gmail.com ) // PGP key: 0x07d1490f82a211a2
+:: History:       1.3.0-TRON - Merge x86 and x64 scripts into one master script
 :: History:       1.2.1-TRON - Remove logging of ftype and assoc output since it's not of any consequence
 ::                1.2.0-TRON - Remove logging functions since Tron handles logging
 ::                1.0.0      + Initial write
@@ -10,7 +11,7 @@
 ::                                     (7z,bz2,bzip2,gz,gzip,lzh,lzma,rar,tar,tgz,zip)
 ::                  associate_all     Associate 7-zip with ALL the file compression formats it supports
 ::
-::                e.g. 7-Zip v9.20 x86.bat associate_all  
+::                e.g. install_7-Zip.bat associate_all  
 ::
 ::                Default is "associate_common" unless told otherwise
 
@@ -19,8 +20,8 @@
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set VERSION=1.2.1-TRON
-set UPDATED=2014-03-06
+set VERSION=1.3.0-TRON
+set UPDATED=2015-10-13
 :: Get the date into ISO 8601 standard date format (yyyy-mm-dd) so we can use it
 FOR /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') DO set DTS=%%a
 set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
