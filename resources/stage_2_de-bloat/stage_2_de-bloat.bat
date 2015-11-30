878103c4b8cab73b8cd995cf468c03ec84afd409
Vocatus Gate <vocatus.gate@gmail.com>
2015-11-30 09:06:47 -0700

    Update stage_2_de-bloat.bat

--- resources/stage_2_de-bloat/stage_2_de-bloat.bat
+++ resources/stage_2_de-bloat/stage_2_de-bloat.bat
@@ -39,7 +39,7 @@ call :log "%CUR_DATE% %TIME%   stage_2_de-bloat begin..."
 :: JOB: Remove crapware programs, phase 1: by specific GUID
 title TRON v%SCRIPT_VERSION% [stage_2_de-bloat] [Remove bloatware by GUID]
 call :log "%CUR_DATE% %TIME%    Attempt junkware removal: Phase 1 (by specific GUID)..."
-call :log "%CUR_DATE% %TIME%    Customize here: \resources\stage_2_de-bloat\oem\programs_to_target_by_GUID.bat"
+call :log "%CUR_DATE% %TIME%    Tailor here: \resources\stage_2_de-bloat\oem\programs_to_target_by_GUID.bat"
 if /i %DRY_RUN%==no call stage_2_de-bloat\oem\programs_to_target_by_GUID.bat >> "%LOGPATH%\%LOGFILE%" 2>&1
 call :log "%CUR_DATE% %TIME%    Done."
 
@@ -47,7 +47,7 @@ call :log "%CUR_DATE% %TIME%    Done."
 :: JOB: Remove crapware programs, phase 2: wildcard by name
 title TRON v%SCRIPT_VERSION% [stage_2_de-bloat] [Remove bloatware by name]
 call :log "%CUR_DATE% %TIME%    Attempt junkware removal: Phase 2 (wildcard by name)..."
-call :log "%CUR_DATE% %TIME%    Customize here: \resources\stage_2_de-bloat\oem\programs_to_target_by_name.txt"
+call :log "%CUR_DATE% %TIME%    Tailor here: \resources\stage_2_de-bloat\oem\programs_to_target_by_name.txt"
 :: Search through the list of programs in "programs_to_target.txt" file and uninstall them one-by-one
 if /i %DRY_RUN%==no FOR /F "tokens=*" %%i in (stage_2_de-bloat\oem\programs_to_target_by_name.txt) DO echo   %%i && echo   %%i...>> "%LOGPATH%\%LOGFILE%" && %WMIC% product where "name like '%%i'" uninstall /nointeractive>> "%LOGPATH%\%LOGFILE%"
 call :log "%CUR_DATE% %TIME%    Done."
@@ -56,7 +56,7 @@ call :log "%CUR_DATE% %TIME%    Done."
 :: JOB: Remove crapware programs, phase 3: unwanted toolbars and BHOs by GUID
 title TRON v%SCRIPT_VERSION% [stage_2_de-bloat] [Remove toolbars by GUID]
 call :log "%CUR_DATE% %TIME%    Attempt junkware removal: Phase 3 (toolbars by specific GUID)..."
-call :log "%CUR_DATE% %TIME%    Customize here: \resources\stage_2_de-bloat\oem\toolbars_BHOs_to_target_by_GUID.bat"
+call :log "%CUR_DATE% %TIME%    Tailor here: \resources\stage_2_de-bloat\oem\toolbars_BHOs_to_target_by_GUID.bat"
 if /i %DRY_RUN%==no call stage_2_de-bloat\oem\toolbars_BHOs_to_target_by_GUID.bat >> "%LOGPATH%\%LOGFILE%" 2>&1
 call :log "%CUR_DATE% %TIME%    Done."
 
