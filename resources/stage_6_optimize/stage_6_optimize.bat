e04b7a8358b73a141435ca7ab5d035be42e7d003
Vocatus Gate <vocatus.gate@gmail.com>
2015-11-30 08:47:02 -0700

    Update stage_6_optimize.bat

--- resources/stage_6_optimize/stage_6_optimize.bat
+++ resources/stage_6_optimize/stage_6_optimize.bat
@@ -11,7 +11,7 @@
 :: PREP AND CHECKS ::
 :::::::::::::::::::::
 set STAGE_6_SCRIPT_VERSION=1.0.0
-set STAGE_6_SCRIPT_DATE=2015-11-23
+set STAGE_6_SCRIPT_DATE=2015-12-xx
 
 :: Quick check to see if we inherited the appropriate variables from Tron.bat
 if /i "%LOGFILE%"=="" (
@@ -33,6 +33,8 @@ if /i "%LOGFILE%"=="" (
 :::::::::::::::::::::::
 :: STAGE 6: Optimize :: // Begin jobs
 :::::::::::::::::::::::
+call :log "%CUR_DATE% %TIME%   stage_6_optimize jobs begin..."
+
 
 :: JOB: Reset the system page file settings
 title TRON v%SCRIPT_VERSION% [stage_6_optimize] [pageFileReset]
@@ -68,7 +70,8 @@ if "%SSD_DETECTED%"=="yes" (
 
 
 
-
+:: Stage complete
+call :log "%CUR_DATE% %TIME%   stage_6_optimize jobs complete."
 
 
 
