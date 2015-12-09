cc8b15080855e10e75f39e77b608071853be96d2
Vocatus Gate <vocatus.gate@gmail.com>
2015-12-09 05:51:12 -0700

    Import comment block explaining function from tron.bat

--- resources/functions/log.bat
+++ resources/functions/log.bat
@@ -1,3 +1,8 @@
+:: Description: Tron's log function
+
+:: %1 reference contains the first argument passed to the function. When the
+:: whole argument string is wrapped in double quotes, it is sent as an argument.
+:: The tilde syntax (%~1) removes the double quotes around the argument.
 @echo off
 echo:%~1 >> "%LOGPATH%\%LOGFILE%"
 echo:%~1
