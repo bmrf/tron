d9768486e148305bb34bcffe0fc5bd1cf1cca093
Vocatus Gate <vocatus.gate@gmail.com>
2016-01-05 08:36:18 -0700

    suppress command output

--- resources/stage_4_repair/purge_windows_telemetry/purge_windows_7-8-81_telemetry.bat
+++ resources/stage_4_repair/purge_windows_telemetry/purge_windows_7-8-81_telemetry.bat
@@ -1,3 +1,4 @@
+@echo off
 cls
-@echo This is only a placeholder file; all Windows 7/8/8.1 telemetry purging code is directly integrated into the main Tron script
+echo This is only a placeholder file; all Windows 7/8/8.1 telemetry purging code is directly integrated into the main Tron script
 pause
