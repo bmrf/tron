e19b1490ee5b64f2287c6cdab27b14974c1d3ac1
Vocatus Gate <vocatus.gate@gmail.com>
2016-02-11 10:08:11 -0700

    Fix numerous bugs with DISM check and repair

--- resources/stage_4_repair/stage_4_repair.bat
+++ resources/stage_4_repair/stage_4_repair.bat
@@ -3,7 +3,8 @@
 ::                2. Safe mode is strongly recommended (though not required)
 ::                3. Called from tron.bat. If you try to run this script directly it will error out
 :: Author:        vocatus on reddit.com/r/TronScript ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
-:: Version:       1.0.5 - Remove redundant Dism image store cleanup (move to Stage 5)
+:: Version:       1.0.6 ! Fix numerous bugs with DISM check and repair. Due to bad ERRORLEVEL checking it would fail to trigger repairs when they were required
+::                1.0.5 - Remove redundant Dism image store cleanup (move to Stage 5)
 ::                1.0.4 - Move Windows 7/8/8.1 telemetry removal code into separate sub-script (similar to Win10)
 ::                1.0.3 - Remove internal log function and switch to Tron's external logging function. Thanks to github:nemchik
 ::                      ! Fix incorrect file name in call to "disable_telemetry_registry_entries.reg"
@@ -18,8 +19,8 @@
 :::::::::::::::::::::
 :: PREP AND CHECKS ::
 :::::::::::::::::::::
-set STAGE_4_SCRIPT_VERSION=1.0.5
-set STAGE_4_SCRIPT_DATE=2016-01-08
+set STAGE_4_SCRIPT_VERSION=1.0.6
+set STAGE_4_SCRIPT_DATE=2016-02-11
 
 :: Quick check to see if we inherited the appropriate variables from Tron.bat
 if /i "%LOGFILE%"=="" (
@@ -45,33 +46,34 @@ call functions\log.bat "%CUR_DATE% %TIME%   stage_4_repair begin..."
 
 
 :: JOB: Check Windows Image for corruptions before running SFC (Windows 8 and up)
-title Tron v%SCRIPT_VERSION% [stage_4_repair] [DISM Check]
-call functions\log.bat "%CUR_DATE% %TIME%    Launch job 'Dism Windows image check (Win8 and up)'..."
-if /i %DRY_RUN%==yes goto skip_dism_image_check
-
-:: Read WIN_VER_NUM and run the scan if we're on 8 or above
 if %WIN_VER_NUM% geq 6.2 (
-	Dism /Online /NoRestart /Cleanup-Image /ScanHealth /Logpath:"%LOGPATH%\tron_dism.log"
-	type "%LOGPATH%\tron_dism.log" >> "%LOGPATH%\%LOGFILE%"
-	del /f /q "%LOGPATH%\tron_dism.log"
+	title Tron v%SCRIPT_VERSION% [stage_4_repair] [DISM Check]
+	call functions\log.bat "%CUR_DATE% %TIME%    Launch job 'DISM Windows image check'..."
+	if /i %DRY_RUN%==yes goto skip_dism_image_check
+	Dism /Online /NoRestart /Cleanup-Image /ScanHealth /Logpath:"%RAW_LOGS%\dism_check.log"
 )
 
 :: If we detect errors try to repair them
-if /i not %ERRORLEVEL%==0 (
-	title Tron v%SCRIPT_VERSION% [stage_3_disinfect] [DISM Repair]
+if not %ERRORLEVEL%==0 (
+	title Tron v%SCRIPT_VERSION% [stage_4_repair] [DISM Repair]
 	if %WIN_VER_NUM% geq 6.2 (
 		call functions\log.bat "%CUR_DATE% %TIME% !  DISM: Image corruption detected. Attempting repair..."
 		:: Add /LimitAccess flag to this command to prevent connecting to Windows Update for replacement files
-		Dism /Online /NoRestart /Cleanup-Image /RestoreHealth /Logpath:"%LOGPATH%\tron_dism.log"
-		type "%LOGPATH%\tron_dism.log" >> "%LOGPATH%\%LOGFILE%"
-	) else (
-		call functions\log.bat "%CUR_DATE% %TIME%    DISM: No image corruption detected."
+		Dism /Online /NoRestart /Cleanup-Image /RestoreHealth /Logpath:"%RAW_LOGS%\dism_repair.log"
 	)
+) else (
+	call functions\log.bat "%CUR_DATE% %TIME%    DISM: No image corruption detected."
+)
+
+:: Add the DISM logs to the main Tron log
+if %WIN_VER_NUM% get 6.2 (
+	call functions\log.bat "%CUR_DATE% %TIME%    Compiling DISM logs into main Tron log..."
+	if exist "%RAW_LOGS%\dism_check.log" type "%RAW_LOGS%\dism_check.log" >> "%LOGPATH%\%LOGFILE%" 2>nul
+	if exist "%RAW_LOGS%\dism_repair.log" type "%RAW_LOGS%\dism_repair.log" >> "%LOGPATH%\%LOGFILE%" 2>nul
 )
 
 :skip_dism_image_check
 call functions\log.bat "%CUR_DATE% %TIME%    Done."
-
 
 
 :: JOB: Reset registry permissions
@@ -103,11 +105,13 @@ if /i %SKIP_FILEPERMS_RESET%==no (
 :: JOB: System File Checker (SFC) scan
 title Tron v%SCRIPT_VERSION% [stage_4_repair] [SFC Scan]
 call functions\log.bat "%CUR_DATE% %TIME%    Launch job 'System File Checker'..."
-if /i %DRY_RUN%==yes goto skip_sfc
-:: Basically this says "If OS is NOT XP or 2003, go ahead and run system file checker." We skip SFC on XP/2k3 because it forces a reboot
-if %WIN_VER_NUM% geq 6.0 %SystemRoot%\System32\sfc.exe /scannow
-%SystemRoot%\System32\findstr.exe /c:"[SR]" %SystemRoot%\logs\cbs\cbs.log>> "%LOGPATH%\%LOGFILE%" 2>NUL
-:skip_sfc
+if /i %DRY_RUN%==no (
+	REM Basically this says "If OS is NOT XP or 2003, go ahead and run system file checker." We skip SFC on XP/2k3 because it forces a reboot
+	if %WIN_VER_NUM% geq 6.0 (
+		%SystemRoot%\System32\sfc.exe /scannow
+		%SystemRoot%\System32\findstr.exe /c:"[SR]" %SystemRoot%\logs\cbs\cbs.log>> "%LOGPATH%\%LOGFILE%" 2>NUL
+	)
+)
 call functions\log.bat "%CUR_DATE% %TIME%    Done."
 
 
@@ -122,14 +126,14 @@ if /i not %ERRORLEVEL%==0 (
 	if /i %DRY_RUN%==no fsutil dirty set %SystemDrive%
 ) else (
 	call functions\log.bat "%CUR_DATE% %TIME%    No errors found on %SystemDrive%. Skipping full chkdsk at next reboot."
-	)
+)
 call functions\log.bat "%CUR_DATE% %TIME%    Done."
 
 
-:: JOB: Kill Microsoft telemetry (user tracking)
+:: JOB: Remove Microsoft telemetry (user tracking)
 title Tron v%SCRIPT_VERSION% [stage_4_repair] [kill-telemetry]
 if /i %SKIP_TELEMETRY_REMOVAL%==yes (
-	call functions\log.bat "%CUR_DATE% %TIME% !  SKIP_TELEMETRY_REMOVAL (-str) set. Disabling Microsoft telemetry (user tracking) instead of purging"
+	call functions\log.bat "%CUR_DATE% %TIME% !  SKIP_TELEMETRY_REMOVAL (-str) set. Disabling instead of removing."
 	REM Only disable telemetry, don't completely purge it
 	reg import stage_4_repair\purge_windows_telemetry\disable_telemetry_registry_entries.reg >nul 2>&1
 	regedit /S stage_4_repair\purge_windows_telemetry\disable_telemetry_registry_entries.reg >nul 2>&1
