b6f38faffb1d1ae91b26bf3294afb4fcaff12fc6
Vocatus Gate <vocatus.gate@gmail.com>
2016-06-27 07:48:40 -0400

    v1.0.3-TRON
    Add Chrome Flash installer code

--- resources/stage_5_patch/adobe/flash_player/Adobe Flash Player Installer.bat
+++ resources/stage_5_patch/adobe/flash_player/Adobe Flash Player Installer.bat
@@ -1,7 +1,8 @@
 :: Purpose:       Installs a package
 :: Requirements:  Run this script with a network admin account
 :: Author:        reddit.com/user/vocatus ( vocatus.gate@gmail.com ) // PGP key: 0x07d1490f82a211a2
-:: Version:       1.0.2-TRON * Make version-agnostic. Now just drop the latest Flash installers, named appropriately, in the same directory as this script
+:: Version:       1.0.3-TRON + Add installer for Chrome version of Flash (PPAPI)
+::                1.0.2-TRON * Make version-agnostic. Now just drop the latest Flash installers, named appropriately, in the same directory as this script
 ::                1.0.1-TRON * Make architecture-agnostic, now will detect correct system architecture and install relevant package
 ::                           * Replace all hard-coded system file paths with relevant variable for better portability
 ::                1.0.0-TRON + Initial build for Tron, modified from PDQ Deploy pack installer version
@@ -21,8 +22,8 @@ set FLAGS=ALLUSERS=1 /q /norestart
 :: Prep :: -- Don't change anything in this section
 ::::::::::
 @echo off
-set SCRIPT_VERSION=1.0.2-TRON
-set SCRIPT_UPDATED=2016-04-17
+set SCRIPT_VERSION=1.0.3-TRON
+set SCRIPT_UPDATED=2016-06-27
 pushd %~dp0
 
 
@@ -33,17 +34,23 @@ pushd %~dp0
 taskkill /f /im firefox.exe /t >> "%LOGPATH%\%LOGFILE%" 2>NUL
 taskkill /f /im palemoon.exe /t >> "%LOGPATH%\%LOGFILE%" 2>NUL
 taskkill /f /im iexplore.exe /t >> "%LOGPATH%\%LOGFILE%" 2>NUL
+taskkill /f /im chrome.exe /t >> "%LOGPATH%\%LOGFILE%" 2>NUL
+taskkill /f /im chrome64.exe /t >> "%LOGPATH%\%LOGFILE%" 2>NUL
 wmic process where name="firefox.exe" call terminate >> "%LOGPATH%\%LOGFILE%" 2>NUL
 wmic process where name="palemoon.exe" call terminate >> "%LOGPATH%\%LOGFILE%" 2>NUL
 wmic process where name="iexplore.exe" call terminate >> "%LOGPATH%\%LOGFILE%" 2>NUL
+wmic process where name="chrome.exe" call terminate >> "%LOGPATH%\%LOGFILE%" 2>NUL
+wmic process where name="chrome64.exe" call terminate >> "%LOGPATH%\%LOGFILE%" 2>NUL
 
 :: Remove prior versions of the Flash player
 wmic product where "name like 'Adobe Flash Player%%Plugin'" uninstall /nointeractive >> "%LOGPATH%\%LOGFILE%" 2>NUL
 wmic product where "name like 'Adobe Flash Player%%NPAPI'" uninstall /nointeractive >> "%LOGPATH%\%LOGFILE%" 2>NUL
+wmic product where "name like 'Adobe Flash Player%%PPAPI'" uninstall /nointeractive >> "%LOGPATH%\%LOGFILE%" 2>NUL
 
 :: Install Firefox and IE versions of Flash
-msiexec /i "install_flash_player_21_plugin.msi" %FLAGS%
-msiexec /i "install_flash_player_21_active_x.msi" %FLAGS%
+msiexec /i "install_flash_player_22_plugin.msi" %FLAGS%
+msiexec /i "install_flash_player_22_active_x.msi" %FLAGS%
+msiexec /i "install_flash_player_22_ppapi.msi" %FLAGS%
 
 :: Delete the Adobe Acrobat Update Service
 net stop AdobeARMservice >> "%LOGPATH%\%LOGFILE%" 2>NUL
