394d1459908495b5620e0927083582052282448a
Vocatus Gate <vocatus.gate@gmail.com>
2016-08-13 10:37:30 -0400

    v1.0.3-TRON

--- resources/stage_4_repair/reset_registry_and_file_permissions/reset_registry_permissions.bat
+++ resources/stage_4_repair/reset_registry_and_file_permissions/reset_registry_permissions.bat
@@ -1,6 +1,7 @@
 :: Purpose:       Restores full rights to Administrator and SYSTEM accounts on everything in specified registry hives
 :: Requirements:  subinacl in the system path or system32 folder
 :: Author:        reddit.com/user/vocatus ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
+:: Version:       1.0.3-TRON * Loopify the subinacl statements to reduce size of the script. No functionality change
 ::                1.0.2-TRON ! Add missing %RAW_LOGS%\ prefix to log compilation line. Thanks to /u/toomasmolder
 ::                1.0.1-TRON ! Add missing pushd statement
 ::                1.0.0-TRON / Modifications for Tron. Remove logging, CUR_DATE, and various other unnecessary code
@@ -19,8 +20,8 @@ SETLOCAL
 ::::::::::
 @echo off
 SETLOCAL
-set SCRIPT_VERSION=1.0.2-TRON
-set SCRIPT_UPDATED=2016-03-21
+set SCRIPT_VERSION=1.0.3-TRON
+set SCRIPT_UPDATED=2016-08-13
 
 :: We need this for Tron because subinacl.exe is in the same directory as this script
 pushd %~dp0
@@ -29,15 +30,15 @@ pushd %~dp0
 :::::::::::::
 :: EXECUTE ::
 :::::::::::::
-subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_HKLM.log /subkeyreg HKEY_LOCAL_MACHINE /grant=administrators=f
-subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_HKCU.log /subkeyreg HKEY_CURRENT_USER /grant=administrators=f
-subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_HKCR.log /subkeyreg HKEY_CLASSES_ROOT /grant=administrators=f
-subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_HKLM1.log /subkeyreg HKEY_LOCAL_MACHINE /grant=system=f
-subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_HKCU1.log /subkeyreg HKEY_CURRENT_USER /grant=system=f
-subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_HKCR1.log /subkeyreg HKEY_CLASSES_ROOT /grant=system=f
+for %%i in (HKEY_LOCAL_MACHINE,HKEY_CURRENT_USER,HKEY_CLASSES_ROOT) do (
+	subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_%%i.log /subkeyreg %%i /grant=administrators=f
+	subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_reg_reset_%%i1.log /subkeyreg %%i /grant=system=f
+)
 
 :: Compile the logs
-type %RAW_LOGS%\subinacl_reg_reset_HKLM1.log >> %RAW_LOGS%\subinacl_reg_reset_HKLM.log
-type %RAW_LOGS%\subinacl_reg_reset_HKCU1.log >> %RAW_LOGS%\subinacl_reg_reset_HKCU.log
-type %RAW_LOGS%\subinacl_reg_reset_HKCR1.log >> %RAW_LOGS%\subinacl_reg_reset_HKCR.log
+for %%i in (HKEY_LOCAL_MACHINE,HKEY_CURRENT_USER,HKEY_CLASSES_ROOT) do (
+	type %RAW_LOGS%\subinacl_reg_reset_%%i1.log >> %RAW_LOGS%\subinacl_reg_reset_%%i.log
+)
+
+
 del /f /q *1.log >NUL 2>&1
