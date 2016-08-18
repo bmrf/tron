aa79899051702444c627081f0f6f736fa5372798
Vocatus Gate <vocatus.gate@gmail.com>
2016-08-18 16:10:53 -0300

    v1.0.2

--- resources/stage_0_prep/check_update/update_check.bat
+++ resources/stage_0_prep/check_update/update_check.bat
@@ -1,7 +1,8 @@
 :: Purpose:       Tron's update checker, broken out from tron.bat as a function
 :: Requirements:  Must be called from Tron
 :: Author:        vocatus on reddit.com/r/TronScript ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
-:: Version:       1.0.1 ! Fix SSL encryption on update check and new version download. Previously we were skipping certificate checking due to cert errors. With this fix we now properly use the bundled .pem certificate to establish an SSL connection to the repo
+:: Version:       1.0.2 + Import REPO_URL, REPO_BTSYNC_KEY, REPO_SCRIPT_DATE and REPO_SCRIPT_VERSION variables from tron.bat since they're only relevant here
+::                1.0.1 ! Fix SSL encryption on update check and new version download. Previously we were skipping certificate checking due to cert errors. With this fix we now properly use the bundled .pem certificate to establish an SSL connection to the repo
 ::                1.0.0 + Initial write
 @echo off
 
@@ -10,8 +11,14 @@
 :::::::::::::::::::::
 :: PREP AND CHECKS ::
 :::::::::::::::::::::
-set UPDATE_CHECK_VERSION=1.0.1
-set UPDATE_CHECK_VERSION=2016-08-12
+set UPDATE_CHECK_VERSION=1.0.2
+set UPDATE_CHECK_VERSION=2016-08-18
+
+:: Variables used during the update check
+set REPO_URL=https://bmrf.org/repos/tron
+set REPO_BTSYNC_KEY=BYQYYECDOJPXYA2ZNUDWDN34O2GJHBM47
+set REPO_SCRIPT_DATE=0
+set REPO_SCRIPT_VERSION=0
 
 :: Quick check to see if we inherited the appropriate variables from Tron.bat
 if /i "%LOGFILE%"=="" (
@@ -29,11 +36,12 @@ if /i "%LOGFILE%"=="" (
 )
 
 
+
 :::::::::::::::::::::::
 :: CHECK FOR UPDATES ::
 :::::::::::::::::::::::
 
-:: Use wget to fetch sha256sums.txt from the repo and parse through it. Extract latest version number and release date from last line (which is always the latest release)
+:: wget sha256sums.txt from the repo and parse through it. Extract latest version number and release date from last line (which is always the latest release)
 stage_0_prep\check_update\wget.exe --ca-certificate=stage_0_prep\check_update\bmrf.org.pem %REPO_URL%/sha256sums.txt -O %TEMP%\sha256sums.txt 2>NUL
 :: Assuming there was no error, go ahead and extract version number into REPO_SCRIPT_VERSION, and release date into REPO_SCRIPT_DATE
 if /i %ERRORLEVEL%==0 (
