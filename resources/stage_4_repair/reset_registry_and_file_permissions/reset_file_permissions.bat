597b878add1f3d051e1ea575388726aee3343010
Vocatus Gate <vocatus.gate@gmail.com>
2016-09-12 09:48:24 -0300

    v1.0.3

--- resources/stage_4_repair/reset_registry_and_file_permissions/reset_file_permissions.bat
+++ resources/stage_4_repair/reset_registry_and_file_permissions/reset_file_permissions.bat
@@ -1,6 +1,10 @@
 :: Purpose:       Restores full rights to Administrator and SYSTEM accounts on everything in the Windows system directory
 :: Requirements:  subinacl in the system path or system32 folder
 :: Author:        reddit.com/user/vocatus ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
+::                1.0.3-TRON * Add proper version-specifc commands to secedit database repair. Was incorrectly attempting to run Windows XP command on Windows 7 and up
+::                           - Remove unecessary SETLOCAL command
+::                           + Add standalone execution support
+::                           * Improve log compilation and filenames to be more descriptive
 ::                1.0.2-TRON ! Add missing %RAW_LOGS%\ prefix to log compilation line. Thanks to /u/toomasmolder
 ::                1.0.1-TRON ! Add missing pushd statement
 ::                1.0.0-TRON / Modifications for Tron. Remove logging, CUR_DATE, and various other unnecessary code
@@ -9,32 +13,50 @@ SETLOCAL
 
 
 :::::::::::::::
-:: VARIABLES ::
+:: VARIABLES :: -------------- These are the defaults. Change them if you so desire. --------- ::
 :::::::::::::::
-:: no user-set variables for this script
+:: No user-set variables for this script
 
 
-::::::::::
-:: Prep :: -- Don't change anything in this section
-::::::::::
+:: --------------------------- Don't edit anything below this line --------------------------- ::
+
+
+:::::::::::::::::::::
+:: PREP AND CHECKS ::
+:::::::::::::::::::::
 @echo off
-SETLOCAL
-set SCRIPT_VERSION=1.0.2-TRON
-set SCRIPT_UPDATED=2016-03-21
+set SCRIPT_VERSION=1.0.3-TRON
+set SCRIPT_UPDATED=2016-09-12
 
-:: We need this for Tron because subinacl.exe is in the same directory as this script
-pushd %~dp0
+:: Only needed for standalone execution since subinacl.exe is in the same directory as this script
+pushd "%~dp0"
+
+:: Populate dependent variables if we didn't inherit them from Tron (standalone execution)
+if /i "%LOGPATH%"=="" (
+	set LOGPATH=%SystemDrive%\Logs
+	set RAW_LOGS=%LOGPATH%\raw_logs
+	for /f "tokens=3*" %%i IN ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName ^| find "ProductName"') DO set WIN_VER=%%i %%j
+	for /f "tokens=3*" %%i IN ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentVersion ^| find "CurrentVersion"') DO set WIN_VER_NUM=%%i
+)
 
 
 :::::::::::::
 :: EXECUTE ::
 :::::::::::::
-subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_filesystem_reset.log /subdirectories %WinDir% /grant=administrators=f
-subinacl /noverbose /outputlog=%RAW_LOGS%\subinacl_filesystem_reset1.log /subdirectories %WinDir% /grant=system=f
+:: Filesystem permissions repair
+subinacl /noverbose /outputlog=%RAW_LOGS%\reset_filesystem_perms_subinacl_Administrators.log /subdirectories %WinDir% /grant=administrators=f
+subinacl /noverbose /outputlog=%RAW_LOGS%\reset_filesystem_perms_subinacl_SYSTEM.log /subdirectories %WinDir% /grant=system=f
 
 :: Security database repair
-secedit /configure /cfg %windir%\repair\secsetup.inf /db secsetup.sdb /verbose >> "%RAW_LOGS%\secedit_filesystem_reset.log" 2>&1
+:: Windows XP/2003
+if %WIN_VER_NUM:~0,1%==5 secedit /configure /cfg %windir%\repair\secsetup.inf /db secsetup.sdb /verbose >> "%RAW_LOGS%\reset_filesystem_perms_secedit.log" 2>&1
+
+:: Windows 7 and up
+if %WIN_VER_NUM:~0,1% geq 6 secedit /configure /cfg %windir%\inf\defltbase.inf /db defltbase.sdb /verbose >> "%RAW_LOGS%\reset_filesystem_perms_secedit.log" 2>&1
 
 :: Compile the logs
-type %RAW_LOGS%\subinacl_filesystem_reset1.log >> %RAW_LOGS%\subinacl_filesystem_reset.log
-del /f /q *1.log >NUL 2>&1
+type %RAW_LOGS%\reset_filesystem_perms_subinacl_Administrators.log >> %RAW_LOGS%\reset_filesystem_perms_subinacl.log
+type %RAW_LOGS%\reset_filesystem_perms_subinacl_SYSTEM.log >> %RAW_LOGS%\reset_filesystem_perms_subinacl.log
+
+
+popd
