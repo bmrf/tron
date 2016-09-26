71eac285e154d154eaf9a4cb7589fe3169c45ce5
Vocatus Gate <vocatus.gate@gmail.com>
2016-09-26 16:30:46 -0300

    v1.0.3-TRON

--- resources/stage_4_repair/reset_registry_and_file_permissions/reset_filesystem_permissions.bat
+++ resources/stage_4_repair/reset_registry_and_file_permissions/reset_filesystem_permissions.bat
@@ -1,10 +1,10 @@
 :: Purpose:       Restores full rights to Administrator and SYSTEM accounts on everything in the Windows system directory
 :: Requirements:  subinacl in the system path or system32 folder
 :: Author:        reddit.com/user/vocatus ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
-::                1.0.3-TRON * Add proper version-specifc commands to secedit database repair. Was incorrectly attempting to run Windows XP command on Windows 7 and up
+::                1.0.3-TRON * Add proper version-specific commands to secedit database repair. Was incorrectly attempting to run Windows XP command on Windows 7 and up
 ::                           + Add /nostatistic flag to subinacl calls. This should suppress the red banner that appears and always made people think something was wrong
 ::                           + Add standalone execution support
-::                           - Remove unecessary SETLOCAL command
+::                           - Remove unnecessary SETLOCAL command
 ::                           - Remove logging of filesystem permissions reset since it only produces a bunch of "access denied" log entries on protected files that we don't care about
 ::                1.0.2-TRON ! Add missing %RAW_LOGS%\ prefix to log compilation line. Thanks to /u/toomasmolder
 ::                1.0.1-TRON ! Add missing pushd statement
@@ -29,8 +29,6 @@ SETLOCAL
 set SCRIPT_VERSION=1.0.3-TRON
 set SCRIPT_UPDATED=2016-09-12
 
-:: Only needed for standalone execution since subinacl.exe is in the same directory as this script
-pushd "%~dp0"
 
 :: Populate dependent variables if we didn't inherit them from Tron (standalone execution)
 if /i "%LOGPATH%"=="" (
@@ -38,6 +36,7 @@ if /i "%LOGPATH%"=="" (
 	set RAW_LOGS=%LOGPATH%\raw_logs
 	for /f "tokens=3*" %%i IN ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName ^| find "ProductName"') DO set WIN_VER=%%i %%j
 	for /f "tokens=3*" %%i IN ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentVersion ^| find "CurrentVersion"') DO set WIN_VER_NUM=%%i
+	pushd "%~dp0"
 )
 
 
@@ -45,8 +44,8 @@ if /i "%LOGPATH%"=="" (
 :: EXECUTE ::
 :::::::::::::
 :: Filesystem permissions repair
-subinacl /noverbose /nostatistic /subdirectories %WinDir% /grant=administrators=f >NUL
-subinacl /noverbose /nostatistic /subdirectories %WinDir% /grant=system=f >NUL
+subinacl.exe /noverbose /nostatistic /subdirectories %WinDir% /grant=administrators=f >%TEMP%\test.log
+subinacl.exe /noverbose /nostatistic /subdirectories %WinDir% /grant=system=f >NUL
 
 :: Security database repair
 :: Windows XP/2003
