a28f41cb03171c8a234408212f5e1776cdf2dfad
TyanColte <TyanColte@gmail.com>
2016-11-15 13:47:01 -0600

    Update check_update.bat
    Changed environmental variable %USERPROFILES% TO %USERPROFILE% to fix file not found error on auto-update.

--- resources/stage_0_prep/check_update/check_update.bat
+++ resources/stage_0_prep/check_update/check_update.bat
@@ -101,7 +101,7 @@ if /i %SCRIPT_VERSION% LSS %REPO_SCRIPT_VERSION% (
 		echo.
 		echo %TIME%   Verifying SHA256 pack integrity, please wait...
 		echo.
-		stage_0_prep\check_update\hashdeep.exe -s -e -b -v -a -k "%TEMP%\sha256sums.txt" "%USERPROFILES%\Desktop\Tron*.exe" | %FIND% /i "Files matched: 1"
+		stage_0_prep\check_update\hashdeep.exe -s -e -b -v -a -k "%TEMP%\sha256sums.txt" "%USERPROFILE%\Desktop\Tron*.exe" | %FIND% /i "Files matched: 1"
 		if !ERRORLEVEL!==0 (
 			color 2f
 			echo %TIME%   SHA256 pack integrity verified. The new version is on your desktop.
