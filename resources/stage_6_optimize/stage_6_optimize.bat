3b9d32c173cba5c9e2f5c1a1f8c940003b90e9b6
Vocatus Gate <vocatus.gate@gmail.com>
2017-02-06 12:44:10 -0700

    v1.0.4

--- resources/stage_6_optimize/stage_6_optimize.bat
+++ resources/stage_6_optimize/stage_6_optimize.bat
@@ -1,9 +1,9 @@
 :: Purpose:       Sub-script containing all commands for Tron's Stage 6: Optimize stage. Called by tron.bat and returns control when finished
 :: Requirements:  1. Administrator access
-::                2. Safe mode is strongly recommended (though not required)
-::                3. Called from tron.bat. If you try to run this script directly it will error out
+::                2. Safe mode is recommended but not required
 :: Author:        vocatus on reddit.com/r/TronScript ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
-:: Version:       1.0.3 * Clarify log messages if we're skipping defrag because of an error
+:: Version:       1.0.4 * script: Update script to support standalone execution
+::                1.0.3 * Clarify log messages if we're skipping defrag because of an error
 ::                1.0.2 + Add support for multiple SKIP_DEFRAG variable values
 ::                1.0.1 - Remove internal log function and switch to Tron's external logging function. Thanks to github:nemchik
 ::                1.0.0 + Initial write
@@ -13,22 +13,16 @@
 :::::::::::::::::::::
 :: PREP AND CHECKS ::
 :::::::::::::::::::::
-set STAGE_6_SCRIPT_VERSION=1.0.3
-set STAGE_6_SCRIPT_DATE=2016-05-15
+set STAGE_6_SCRIPT_VERSION=1.0.4
+set STAGE_6_SCRIPT_DATE=2017-02-04
 
-:: Quick check to see if we inherited the appropriate variables from Tron.bat
+:: Check for standalone vs. Tron execution and build the environment if running in standalone mode
 if /i "%LOGFILE%"=="" (
-	color 0c
-	echo.
-	echo  ERROR
-	echo.
-	echo   You cannot run this script directly - it must be
-	echo   called from Tron.bat during a Tron run.
-	echo.
-	echo   Navigate to Tron's root folder and execute Tron.bat
-	echo.
-	pause
-	exit /b 1
+	:: Load the settings file
+	call functions\tron_settings.bat
+
+	:: Initialize the runtime environment
+	call functions\initialize_environment.bat
 )
 
 
@@ -40,7 +34,7 @@ call functions\log.bat "%CUR_DATE% %TIME%   stage_6_optimize begin..."
 
 
 :: JOB: Reset the system page file settings
-title Tron v%SCRIPT_VERSION% [stage_6_optimize] [pageFileReset]
+title Tron v%TRON_VERSION% [stage_6_optimize] [PageFileReset]
 if /i %SKIP_PAGEFILE_RESET%==yes (
 	call functions\log.bat "%CUR_DATE% %TIME% !  SKIP_PAGEFILE_RESET (-spr) set. Skipping page file reset."
 ) else (
@@ -59,7 +53,7 @@ if /i "%SKIP_DEFRAG%"=="yes_error" (
 	set WARNINGS_DETECTED=yes
 )
 if /i "%SKIP_DEFRAG%"=="no" (
-	title Tron v%SCRIPT_VERSION% [stage_6_optimize] [Defrag]
+	title Tron v%TRON_VERSION% [stage_6_optimize] [Defrag]
 	call functions\log.bat "%CUR_DATE% %TIME%    Launch job 'Defrag %SystemDrive%'..."
 	if /i %DRY_RUN%==no stage_6_optimize\defrag\defraggler.exe %SystemDrive% /MinPercent 5
 	call functions\log.bat "%CUR_DATE% %TIME%    Done."
