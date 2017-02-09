c024234dac06e60a0bed110e5e97d3f7f614ab38
Vocatus Gate <vocatus.gate@gmail.com>
2017-02-09 11:03:30 -0700

    v10.0.0

--- resources/functions/initialize_environment.bat
+++ resources/functions/initialize_environment.bat
@@ -1,4 +1,4 @@
-:: Purpose:       Initializes Tron's environment. Called by Tron.bat at initial launch or by the individual sub-stage scripts when launched manually
+:: Purpose:       Initializes Tron's runtime environment. Called by Tron.bat at initial launch or by and individual sub-stage script when launched manually
 ::                This file determines Tron's overall project version and date
 ::                Do not edit this script
 :: Requirements:  The ability to look and not touch
@@ -8,8 +8,7 @@
 
 :: Tron Project version and date. These two variables determine the overall project version and date 
 set TRON_VERSION=10.0.0
-set TRON_DATE=2017-02-xx
-
+set TRON_DATE=2017-02-09
 
 :: Initialize script-internal variables
 set ERRORS_DETECTED=no
@@ -26,9 +25,9 @@ if /i "%SAFEBOOT_OPTION%"=="NETWORK" set SAFE_MODE=yes
 set SKIP_CHECK_UPDATE=no
 
 :: Force path to some system utilities in case the system PATH is messed up
-	set WMIC=%SystemRoot%\System32\wbem\wmic.exe
-	set FIND=%SystemRoot%\System32\find.exe
-	set FINDSTR=%SystemRoot%\System32\findstr.exe
+set WMIC=%SystemRoot%\System32\wbem\wmic.exe
+set FIND=%SystemRoot%\System32\find.exe
+set FINDSTR=%SystemRoot%\System32\findstr.exe
 
 :: Get the date into ISO 8601 standard format (yyyy-mm-dd) so we can use it
 for /f %%a in ('%WMIC% OS GET LocalDateTime ^| %FIND% "."') DO set DTS=%%a
@@ -38,20 +37,25 @@ set CUR_DATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
 for /f "USEBACKQ skip=1 delims=" %%i IN (`%WMIC% timezone get StandardName ^|findstr /b /r [a-z]`) DO set TIME_ZONE_NAME=%%i
 
 :: Resume-related stuff (resuming from an interrupted run)
-	set RESUME_STAGE=0
-	set RESUME_FLAGS=0
-	set RESUME_DETECTED=no
-	reg query HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce\ /v "tron_resume" >nul 2>&1
-	if %ERRORLEVEL%==0 set RESUME_DETECTED=yes
-	if /i "%1"=="-resume" set RESUME_DETECTED=yes
+set RESUME_STAGE=0
+set RESUME_FLAGS=0
+set RESUME_DETECTED=no
+reg query HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce\ /v "tron_resume" >nul 2>&1
+if %ERRORLEVEL%==0 set RESUME_DETECTED=yes
+if /i "%1"=="-resume" set RESUME_DETECTED=yes
 
 :: Detect the version of Windows we're on. This determines a few things later on
-	set WIN_VER=undetected
-	set WIN_VER_NUM=undetected
-	for /f "tokens=3*" %%i IN ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName ^| %FIND% "ProductName"') DO set WIN_VER=%%i %%j
-	for /f "tokens=3*" %%i IN ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentVersion ^| %FIND% "CurrentVersion"') DO set WIN_VER_NUM=%%i
+set WIN_VER=undetected
+set WIN_VER_NUM=undetected
+for /f "tokens=3*" %%i IN ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName ^| %FIND% "ProductName"') DO set WIN_VER=%%i %%j
+for /f "tokens=3*" %%i IN ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentVersion ^| %FIND% "CurrentVersion"') DO set WIN_VER_NUM=%%i
 
 :: Build USERPROFILES variable which works across ALL versions of Windows for determining location of C:\Users or C:\Documents and Settings
-	pushd "%USERPROFILE%\.."
-	set USERPROFILES=%CD%
-	popd
+pushd "%USERPROFILE%\.."
+set USERPROFILES=%CD%
+popd
+	
+:: Build log directories if they don't already exist
+for %%D in ("%LOGPATH%","%QUARANTINE_PATH%","%BACKUPS%","%RAW_LOGS%","%SUMMARY_LOGS%") do (
+	if not exist %%D mkdir %%D
+)
