aad78df0bc378342aa88844ff233df726139978a
Vocatus Gate <vocatus.gate@gmail.com>
2017-02-09 13:43:08 -0700

    v1.0.3

--- resources/stage_2_de-bloat/metro/metro_Microsoft_modern_apps_to_target_by_name.ps1
+++ resources/stage_2_de-bloat/metro/metro_Microsoft_modern_apps_to_target_by_name.ps1
@@ -4,7 +4,8 @@ Purpose:       Script to remove many of the pre-loaded Microsoft Metro "modern a
 Requirements:  1. Administrator access
                2. Windows 7 and up
 Author:        vocatus on reddit.com/r/TronScript ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
-Version:       1.0.2 - Remove Calendar and Mail app from active target list. Thanks to /u/Reynbou
+Version:       1.0.3 ! Fix typo in $PackagesToRemove array. Thanks to 
+               1.0.2 - Remove Calendar and Mail app from active target list. Thanks to /u/Reynbou
                1.0.1 + Add script version and date variables to support automatic updates at Tron runtime
 #>
 $ErrorActionPreference = "SilentlyContinue"
@@ -13,8 +14,8 @@ $ErrorActionPreference = "SilentlyContinue"
 ########
 # PREP #
 ########
-$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_VERSION = "1.0.2"
-$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_DATE = "2017-02-08"
+$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_VERSION = "1.0.3"
+$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_DATE = "2017-02-09"
 
 
 ###########
@@ -60,7 +61,7 @@ $PackagesToRemove = (
     'Microsoft.ZuneVideo',                      # 'Groove Music' app
     'MicrosoftMahjong',                         # 'Mahjong' game
     'Windows.ContactSupport',                   
-    'king.com.CandyCrushSodaSaga',              # Candy Crush app
+    'king.com.CandyCrushSodaSaga'               # Candy Crush app
 )
 
 # Inactive identifers
