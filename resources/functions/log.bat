20c449047d6804adb01b7e79172b7c46de773626
Vocatus Gate <vocatus.gate@gmail.com>
2017-03-01 14:44:43 -0700

    Update log.bat

--- resources/functions/log.bat
+++ resources/functions/log.bat
@@ -1,8 +1,7 @@
 :: Tron's log function
 
-:: %1 reference contains the first argument passed to the function. When the
-:: whole argument string is wrapped in double quotes, it is sent as an argument.
+:: When the whole argument (%1) string is wrapped in double quotes, it is sent as an argument.
 :: The tilde syntax (%~1) removes the double quotes around the argument.
 @echo off
-echo:%~1 >> "%LOGPATH%\%LOGFILE%"
+echo:%~1>> "%LOGPATH%\%LOGFILE%"
 echo:%~1
