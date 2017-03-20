977ba9eceae95bdbbec221db36d593e4bf34fca5
madbomb122 <madbomb122@users.noreply.github.com>
2017-03-20 15:44:47 -0700

    Update metro_Microsoft_modern_apps_to_target_by_name.ps1

--- resources/stage_2_de-bloat/metro/metro_Microsoft_modern_apps_to_target_by_name.ps1
+++ resources/stage_2_de-bloat/metro/metro_Microsoft_modern_apps_to_target_by_name.ps1
@@ -17,69 +17,70 @@ $ErrorActionPreference = "SilentlyContinue"
 $METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_VERSION = "1.0.3"
 $METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_DATE = "2017-02-09"
 
+Function Remove-App([String]$AppName){
+    $PackageFullName = (Get-AppxPackage $AppName).PackageFullName
+    $ProPackageFullName = (Get-AppxProvisionedPackage -online | where {$_.Displayname -eq $AppName}).PackageName
+    Remove-AppxPackage -package $PackageFullName | Out-Null
+    Remove-AppxProvisionedPackage -online -packagename $ProPackageFullName | Out-Null
+}
 
 ###########
 # EXECUTE #
 ###########
 # Active identifiers
-$PackagesToRemove = (
-    '9E2F88E3.Twitter',                         # Twitter app
-    'D52A8D61.FarmVille2CountryEscape',         
-    'GAMELOFTSA.Asphalt8Airborne',              
-    'Microsoft.3DBuilder',                      # '3DBuilder' app
-    'Microsoft.Advertising.Xaml',               
-    'Microsoft.BingFinance',                    # 'Money' app - Financial news
-    'Microsoft.BingFoodAndDrink',               # 'Food and Drink' app
-    'Microsoft.BingHealthAndFitness',           # 'Health and Fitness' app
-    'Microsoft.BingNews',                       # 'Generic news' app
-    'Microsoft.BingSports',                     # 'Sports' app - Sports news
-    'Microsoft.BingTranslator',                 # 'Translator' app - Bing Translate
-    'Microsoft.BingTravel',                     # 'Travel' app
-    'Microsoft.CommsPhone',                     # 'Phone' app
-    'Microsoft.ConnectivityStore',              
-    'Microsoft.FreshPaint',                     # 'Canvas' app
-    'Microsoft.Getstarted',                     # 'Get Started' link
-    'Microsoft.Messaging',                      # 'Messaging' app
-    'Microsoft.MicrosoftJackpot',               # 'Jackpot' app
-    'Microsoft.MicrosoftJigsaw',                # 'Jigsaw' app
-    'Microsoft.MicrosoftOfficeHub',             
-    'Microsoft.MicrosoftSudoku',                
-    'Microsoft.MovieMoments',                   # imported from stage_2_de-bloat.bat
-    'Microsoft.Office.OneNote',                 # 'Onenote' app
-    'Microsoft.Office.Sway',                    # 'Sway' app
-    'Microsoft.People',                         # 'People' app
-    'Microsoft.SkypeApp',                       # 'Get Skype' link
-    'Microsoft.SkypeWiFi',                      
-    'Microsoft.Studios.Wordament',              # imported from stage_2_de-bloat.bat
-    'Microsoft.Taptiles',                       # imported from stage_2_de-bloat.bat
-    'Microsoft.Windows.CloudExperienceHost',    # 'Cloud Experience' sigh
-    'Microsoft.WindowsFeedback',                # 'Feedback' functionality
-    'Microsoft.WindowsFeedbackHub',             # 'Feedback' functionality
-    'Microsoft.WindowsPhone',                   # 'Phone Companion' app
-    'Microsoft.XboxApp',                        # Xbox junk, unfortunately 'Microsoft.XboxGameCallableUI' and 'Microsoft.XboxIdentityProvider' can't be removed
-    'Microsoft.ZuneMusic',                      # 'Groove Music' app
-    'Microsoft.ZuneVideo',                      # 'Groove Music' app
-    'MicrosoftMahjong',                         # 'Mahjong' game
-    'Windows.ContactSupport',                   
-    'king.com.CandyCrushSodaSaga'               # Candy Crush app
-)
+Remove-App "9E2F88E3.Twitter"                          # Twitter app
+Remove-App "Facebook.Facebook"                         # Facebook app
+Remove-App "D52A8D61.FarmVille2CountryEscape"          
+Remove-App "GAMELOFTSA.Asphalt8Airborne"               
+Remove-App "Microsoft.3DBuilder"                       # '3DBuilder' app
+Remove-App "Microsoft.Advertising.Xaml"                
+Remove-App "Microsoft.BingFinance"                     # 'Money' app - Financial news
+Remove-App "Microsoft.BingFoodAndDrink"                # 'Food and Drink' app
+Remove-App "Microsoft.BingHealthAndFitness"            # 'Health and Fitness' app
+Remove-App "Microsoft.BingNews"                        # 'Generic news' app
+Remove-App "Microsoft.BingSports"                      # 'Sports' app - Sports news
+Remove-App "Microsoft.BingTranslator"                  # 'Translator' app - Bing Translate
+Remove-App "Microsoft.BingTravel"                      # 'Travel' app
+Remove-App "Microsoft.CommsPhone"                      # 'Phone' app
+Remove-App "Microsoft.ConnectivityStore"               
+Remove-App "Microsoft.FreshPaint"                      # 'Canvas' app
+Remove-App "Microsoft.Getstarted"                      # 'Get Started' link
+Remove-App "Microsoft.Messaging"                       # 'Messaging' app
+Remove-App "Microsoft.MicrosoftJackpot"                # 'Jackpot' app
+Remove-App "Microsoft.MicrosoftJigsaw"                 # 'Jigsaw' app
+Remove-App "Microsoft.MicrosoftOfficeHub"              
+Remove-App "Microsoft.MicrosoftSudoku"                 
+Remove-App "Microsoft.MovieMoments"                    # imported from stage_2_de-bloat.bat
+Remove-App "Microsoft.Office.OneNote"                  # 'Onenote' app
+Remove-App "Microsoft.Office.Sway"                     # 'Sway' app
+Remove-App "Microsoft.People"                          # 'People' app
+Remove-App "Microsoft.SkypeApp"                        # 'Get Skype' link
+Remove-App "Microsoft.SkypeWiFi"                       
+Remove-App "Microsoft.Studios.Wordament"               # imported from stage_2_de-bloat.bat
+Remove-App "Microsoft.Taptiles"                        # imported from stage_2_de-bloat.bat
+Remove-App "Microsoft.Windows.CloudExperienceHost"     # 'Cloud Experience' sigh
+Remove-App "Microsoft.WindowsFeedback"                 # 'Feedback' functionality
+Remove-App "Microsoft.WindowsFeedbackHub"              # 'Feedback' functionality
+Remove-App "Microsoft.WindowsPhone"                    # 'Phone Companion' app
+Remove-App "Microsoft.XboxApp"                         # Xbox junk  unfortunately 'Microsoft.XboxGameCallableUI' and 'Microsoft.XboxIdentityProvider' can't be removed
+Remove-App "Microsoft.ZuneMusic"                       # 'Groove Music' app
+Remove-App "Microsoft.ZuneVideo"                       # 'Groove Music' app
+Remove-App "MicrosoftMahjong"                          # 'Mahjong' game
+Remove-App "Windows.ContactSupport"                    
+Remove-App "king.com.CandyCrushSodaSaga"               # Candy Crush app
 
 # Inactive identifers
-    # 'Microsoft.Appconnector',                 # Not sure about this one
-    # 'Microsoft.BingWeather',                  # 'Weather' app
-    # 'Microsoft.BioEnrollment',                # not sure about this one
-    # 'Microsoft.MicrosoftSolitaireCollection', # Solitaire collection
-    # 'Microsoft.Windows.Photos',               # Photos app
-    # 'Microsoft.WindowsAlarms',                # 'Alarms and Clock' app
-    # 'Microsoft.WindowsCalculator',            # Calculator app
-    # 'Microsoft.WindowsCamera',                # Camera app
-    # 'microsoft.windowscommunicationsapps'     # 'Calendar and Mail' app
-    # 'Microsoft.WindowsMaps',                  # Maps app
-    # 'Microsoft.WindowsSoundRecorder',         # Sound Recorder app
-    # 'Microsoft.WindowsStore',                 # Windows Store
+#Remove-App "Microsoft.Appconnector"                   # Not sure about this one
+#Remove-App "Microsoft.BingWeather"                    # 'Weather' app
+#Remove-App "Microsoft.BioEnrollment"                  # not sure about this one
+#Remove-App "Microsoft.MicrosoftSolitaireCollection"   # Solitaire collection
+#Remove-App "Microsoft.Windows.Photos"                 # Photos app
+#Remove-App "Microsoft.WindowsAlarms"                  # 'Alarms and Clock' app
+#Remove-App "Microsoft.WindowsCalculator"              # Calculator app
+#Remove-App "Microsoft.WindowsCamera"                  # Camera app
+#Remove-App "Microsoft.windowscommunicationsapps"      # 'Calendar and Mail' app
+#Remove-App "Microsoft.WindowsMaps"                    # Maps app
+#Remove-App "Microsoft.WindowsSoundRecorder"           # Sound Recorder app
+#Remove-App "Microsoft.WindowsStore"                   # Windows Store
 
-
-
-# Do the removal
-Get-AppxProvisionedPackage -Online | Where-Object Name -In $PackagesToRemove | Remove-AppxProvisionedPackage -Online | Out-Null
-Get-AppxPackage -AllUsers | Where-Object Name -In $PackagesToRemove | Remove-AppxPackage | Out-Null
+Read-Host "PAUSE"
