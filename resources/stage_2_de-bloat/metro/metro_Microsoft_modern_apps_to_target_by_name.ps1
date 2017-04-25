051eaaeab9f5405cb113782854fa3fcf787c2a2d
Vocatus Gate <vocatus.gate@gmail.com>
2017-04-24 17:13:14 -0700

    v1.1.3

--- resources/stage_2_de-bloat/metro/metro_Microsoft_modern_apps_to_target_by_name.ps1
+++ resources/stage_2_de-bloat/metro/metro_Microsoft_modern_apps_to_target_by_name.ps1
@@ -4,7 +4,8 @@ Purpose:       Script to remove many of the pre-loaded Microsoft Metro "modern a
 Requirements:  1. Administrator access
                2. Windows 7 and up
 Author:        vocatus on reddit.com/r/TronScript ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
-Version:       1.1.2 + Add Microsoft.WindowsFeedbackHub to active apps. Thanks to /u/Phaellow
+Version:       1.1.3 + Add various entries
+               1.1.2 + Add Microsoft.WindowsFeedbackHub to active apps. Thanks to /u/Phaellow
                      + Add Microsoft.Microsoft3DViewer to inactive apps. Thanks to /u/Phaellow
                1.1.1 + Add missing entries Microsoft.OneConnect, Microsoft.WindowsReadingList. Thanks to /u/1nfestissumam
                      + Add Microsoft.MicrosoftStickyNotes to disabled list (user request)
@@ -21,8 +22,8 @@ $ErrorActionPreference = "SilentlyContinue"
 ########
 # PREP #
 ########
-$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_VERSION = "1.1.2"
-$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_DATE = "2017-04-13"
+$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_VERSION = "1.1.3"
+$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_DATE = "2017-04-24"
 
 # Build the removal function
 Function Remove-App([String]$AppName){
@@ -36,11 +37,11 @@ Function Remove-App([String]$AppName){
 # EXECUTE #
 ###########
 # Active identifiers
-Remove-App "D52A8D61.FarmVille2CountryEscape"          
-Remove-App "Facebook.Facebook"                         # Facebook app
-Remove-App "GAMELOFTSA.Asphalt8Airborne"               
+Remove-App "*Microsoft.MinecraftUWP*"
+Remove-App "*Microsoft.NetworkSpeedTest*"
+Remove-App "*Microsoft.WindowsReadingList*"
 Remove-App "Microsoft.3DBuilder"                       # '3DBuilder' app
-Remove-App "Microsoft.Advertising.Xaml"                
+Remove-App "Microsoft.Advertising.Xaml"
 Remove-App "Microsoft.BingFinance"                     # 'Money' app - Financial news
 Remove-App "Microsoft.BingFoodAndDrink"                # 'Food and Drink' app
 Remove-App "Microsoft.BingHealthAndFitness"            # 'Health and Fitness' app
@@ -49,36 +50,39 @@ Remove-App "Microsoft.BingSports"                      # 'Sports' app - Sports n
 Remove-App "Microsoft.BingTranslator"                  # 'Translator' app - Bing Translate
 Remove-App "Microsoft.BingTravel"                      # 'Travel' app
 Remove-App "Microsoft.CommsPhone"                      # 'Phone' app
-Remove-App "Microsoft.ConnectivityStore"               
+Remove-App "Microsoft.ConnectivityStore"
 Remove-App "Microsoft.FreshPaint"                      # 'Canvas' app
 Remove-App "Microsoft.Getstarted"                      # 'Get Started' link
 Remove-App "Microsoft.Messaging"                       # 'Messaging' app
 Remove-App "Microsoft.MicrosoftJackpot"                # 'Jackpot' app
 Remove-App "Microsoft.MicrosoftJigsaw"                 # 'Jigsaw' app
-Remove-App "Microsoft.MicrosoftOfficeHub"              
-Remove-App "Microsoft.MicrosoftSudoku"                 
+Remove-App "Microsoft.MicrosoftOfficeHub"
+Remove-App "Microsoft.MicrosoftSudoku"
 Remove-App "Microsoft.MovieMoments"                    # imported from stage_2_de-bloat.bat
 Remove-App "Microsoft.Office.OneNote"                  # 'Onenote' app
 Remove-App "Microsoft.Office.Sway"                     # 'Sway' app
 Remove-App "Microsoft.OneConnect"                      # 'OneConnect' app
 Remove-App "Microsoft.People"                          # 'People' app
 Remove-App "Microsoft.SkypeApp"                        # 'Get Skype' link
-Remove-App "Microsoft.SkypeWiFi"                       
+Remove-App "Microsoft.SkypeWiFi"
 Remove-App "Microsoft.Studios.Wordament"               # imported from stage_2_de-bloat.bat
 Remove-App "Microsoft.WindowsFeedbackHub"              # Feedback app
 
 # Inactive identifers
-#Remove-App "Microsoft.Microsoft3DViewer"              # 3D model viewer
+#Remove-App "*Microsoft.XboxApp*"
+#Remove-App "*Microsoft.ZuneMusic*"
+#Remove-App "*Microsoft.ZuneVideo*"
 #Remove-App "Microsoft.Appconnector"                   # Not sure about this one
 #Remove-App "Microsoft.BingWeather"                    # 'Weather' app
 #Remove-App "Microsoft.BioEnrollment"                  # not sure about this one
-#Remove-App "Microsoft.MicrosoftStickyNotes"           # Pulled from active list due to user requests
+#Remove-App "Microsoft.Microsoft3DViewer"              # 3D model viewer
 #Remove-App "Microsoft.MicrosoftSolitaireCollection"   # Solitaire collection
+#Remove-App "Microsoft.MicrosoftStickyNotes"           # Pulled from active list due to user requests
 #Remove-App "Microsoft.Windows.Photos"                 # Photos app
 #Remove-App "Microsoft.WindowsAlarms"                  # 'Alarms and Clock' app
 #Remove-App "Microsoft.WindowsCalculator"              # Calculator app
 #Remove-App "Microsoft.WindowsCamera"                  # Camera app
-#Remove-App "Microsoft.windowscommunicationsapps"      # 'Calendar and Mail' app
 #Remove-App "Microsoft.WindowsMaps"                    # Maps app
 #Remove-App "Microsoft.WindowsSoundRecorder"           # Sound Recorder app
 #Remove-App "Microsoft.WindowsStore"                   # Windows Store
+#Remove-App "Microsoft.windowscommunicationsapps"      # 'Calendar and Mail' app
