8262b9103578b5b2de475fb8eec8550579743116
Vocatus Gate <vocatus.gate@gmail.com>
2017-06-10 10:32:35 -0700

    Update initialize_environment.bat

--- resources/functions/initialize_environment.bat
+++ resources/functions/initialize_environment.bat
@@ -3,12 +3,12 @@
 ::                Do not edit this script
 :: Requirements:  The ability to look and not touch
 :: Author:        vocatus on reddit.com/r/TronScript ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
-:: Version:       10.0.5 . Increment version number
+:: Version:       10.1.0 . Increment version number
 
 
 :: Tron Project version and date. These two variables determine the overall project version and date 
-set TRON_VERSION=10.0.5
-set TRON_DATE=2017-04-29
+set TRON_VERSION=10.1.0
+set TRON_DATE=2017-06-10
 
 :: Initialize script-internal variables
 set ERRORS_DETECTED=no
