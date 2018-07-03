82e0f882bcb4a7ec27ca53ea707a78acbb535e7d
Vocatus Gate <vocatus.gate@gmail.com>
2018-07-03 19:21:25 +0000

    hardcode path to reg.exe

--- resources/stage_4_repair/repair_file_extensions/repair_file_extensions.bat
+++ resources/stage_4_repair/repair_file_extensions/repair_file_extensions.bat
@@ -1,11 +1,11 @@
-:: Repair Default File Extensions for Windows 7
+:: Repair Default File Extensions
 :: by: /u/cuddlychops06
 :: modified for use in Tron project by /u/vocatus
 
 @echo off
 for %%i in (*.reg) do (
 	echo Importing: %%i >> %LOGPATH%\%LOGFILE%
-	reg import %%i >> %LOGPATH%\%LOGFILE%
+	%SystemRoot%\System32\reg.exe import %%i >> %LOGPATH%\%LOGFILE%
 )
 
 :: Return exit code to SCCM/PDQ Deploy/Tron/etc
