67a0240ea61efa58af38c2686679860d30fd8f3b
Vocatus Gate <vocatus.gate@gmail.com>
2018-07-08 15:28:50 +0000

    Fix for MBAM not launching if it was already installed
    Thanks to [u/b_sen](https://www.reddit.com/user/b_sen)

--- resources/stage_3_disinfect/stage_3_disinfect.bat
+++ resources/stage_3_disinfect/stage_3_disinfect.bat
@@ -2,20 +2,21 @@
 :: Requirements:  1. Administrator access
 ::                2. Safe mode is recommended but not required
 :: Author:        vocatus on reddit.com/r/TronScript ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
-:: Version:       1.2.0 * sophos:      Prevent Sophos from trying to update if a network connection isn't detected
+:: Version:       1.2.1 ! mbam:        Fix for MBAM not launching if it was already installed. Thanks to u/b_sen
+::                1.2.0 * sophos:      Prevent Sophos from trying to update if a network connection isn't detected
 ::                1.1.9 * Improve standalone execution support. Can now execute by double-clicking icon vs. manually executing via CLI
-::                1.1.8 * Update date/time logging functions to use new log_with_date.bat. Thanks to /u/DudeManFoo
+::                1.1.8 * Update date/time logging functions to use new log_with_date.bat. Thanks to u/DudeManFoo
 ::                1.1.7 * mbam:        Improve pre-existing installation detection. Thanks to github:RedBaron2
 ::                1.1.6 * script:      Update script to support standalone execution
 ::                1.1.5 ! mbam:        Fix MBAM not launching or installing bug
-::                1.1.4 * mbam:        Update MBAM detection to include new v3.x series. Thanks to /u/Phantop
+::                1.1.4 * mbam:        Update MBAM detection to include new v3.x series. Thanks to u/Phantop
 ::                1.1.3 + certcache:   Add job to clear the CryptNet SSL certificate cache (Vista and up). Thanks to github:Itsnothectic and github:alazare619
 ::                1.1.2 + jrt:         Add job "JRT" (Junkware Removal Tool by Malwarebytes). Currently disabled (pending troubleshooting)
 ::                      - roguekiller: Remove obsolete code for RogueKiller
 ::                1.1.1 ! mbam:        Clean up mbam launching routine. Should eliminate erroneous message about mbam.exe not being found
 ::                      ! Correct a reference to USERPROFILE that should've used Tron's USERPROFILES instead
-::                1.1.0 + mbam: Add installation of mbam2-rules.exe (offline definition file) to MBAM installation routine. Thanks to /u/sofakingdead
-::                1.0.2 * mbam: Import pre-configured settings.conf that ticks the "scan for rootkits" option when installing MBAM. Thanks to /u/staticextasy
+::                1.1.0 + mbam: Add installation of mbam2-rules.exe (offline definition file) to MBAM installation routine. Thanks to u/sofakingdead
+::                1.0.2 * mbam: Import pre-configured settings.conf that ticks the "scan for rootkits" option when installing MBAM. Thanks to u/staticextasy
 ::                1.0.1 - Remove internal log function and switch to Tron's external logging function. Thanks to github:nemchik
 ::                1.0.0 + Initial write
 @echo off
@@ -24,8 +25,8 @@
 :::::::::::::::::::::
 :: PREP AND CHECKS ::
 :::::::::::::::::::::
-set STAGE_3_SCRIPT_VERSION=1.2.0
-set STAGE_3_SCRIPT_DATE=2018-02-06
+set STAGE_3_SCRIPT_VERSION=1.2.1
+set STAGE_3_SCRIPT_DATE=2018-07-08
 
 :: Check for standalone vs. Tron execution and build the environment if running in standalone mode
 if /i "%LOGFILE%"=="" (
@@ -93,11 +94,12 @@ if /i %SKIP_MBAM_INSTALL%==yes (
 		stage_3_disinfect\mbam\mbam2-rules.exe /sp- /verysilent /suppressmsgboxes /log="%RAW_LOGS%\mbam_rules_install.log" /norestart
 		type "%RAW_LOGS%\mbam_rules_install.log" >> "%LOGPATH%\%LOGFILE%"
 		call functions\log_with_date.bat "    Done."
-
-		:: Scan for and launch appropriate architecture version
-		if exist "%ProgramFiles(x86)%\Malwarebytes Anti-Malware\mbam.exe" start "" "%ProgramFiles(x86)%\Malwarebytes Anti-Malware\mbam.exe"
-		if exist "%ProgramFiles%\Malwarebytes Anti-Malware\mbam.exe" start "" "%ProgramFiles%\Malwarebytes Anti-Malware\mbam.exe"
 	)
+
+	:: Scan for and launch appropriate architecture version
+	if exist "%ProgramFiles(x86)%\Malwarebytes Anti-Malware\mbam.exe" start "" "%ProgramFiles(x86)%\Malwarebytes Anti-Malware\mbam.exe"
+	if exist "%ProgramFiles%\Malwarebytes Anti-Malware\mbam.exe" start "" "%ProgramFiles%\Malwarebytes Anti-Malware\mbam.exe"
+
 	call functions\log_with_date.bat "   Done."
 	call functions\log_with_date.bat "!  NOTE: You must manually click SCAN in the MBAM window!"
 )
