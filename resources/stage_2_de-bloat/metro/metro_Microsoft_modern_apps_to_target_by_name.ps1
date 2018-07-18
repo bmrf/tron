402fa3c47abd6bfc7c69548abbaa657055e380f6
Vocatus Gate <vocatus.gate@gmail.com>
2018-07-18 20:41:29 +0000

    Update metro_Microsoft_modern_apps_to_target_by_name.ps1

--- resources/stage_2_de-bloat/metro/metro_Microsoft_modern_apps_to_target_by_name.ps1
+++ resources/stage_2_de-bloat/metro/metro_Microsoft_modern_apps_to_target_by_name.ps1
@@ -1,12 +1,28 @@
-<#
-Purpose:       Script to remove many of the pre-loaded 3rd-party Metro "modern app" bloatware. Called by Tron in Stage 2: De-bloat
+s<#
+Purpose:       Script to remove many of the pre-loaded Microsoft Metro "modern app" bloatware. Called by Tron in Stage 2: De-bloat
                Add any AppX uninstall commands to this list to target them for removal
 Requirements:  1. Administrator access
                2. Windows 8 and up
 Author:        vocatus on reddit.com/r/TronScript ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
-Version:       1.2.5 + Add additional user-submitted entries
-               1.2.4 * Fixed variable re-use, thanks to github:madbomb22
-               1.2.3 * Implement removal process improvements, thanks to github:madbomb22			   
+Version:       1.2.2 + Add additional user-submitted entries
+               1.2.1 * Fixed variable re-use, thanks to github:madbomb22
+               1.2.0 * Implement removal process improvements, thanks to github:madbomb22
+               1.1.8 + Add additional user-submitted entries
+               1.1.7 + Add Microsoft.GetHelp
+               1.1.6 ! Fix function evalution of * character. Thanks to u/madbomb122 and u/phant0md
+               1.1.5 + Add Zune entries, thanks to /u/ComputeGuy
+               1.1.4 + Add various entries, thanks to github:kronflux
+               1.1.3 + Add various entries
+               1.1.2 + Add Microsoft.WindowsFeedbackHub to active apps. Thanks to /u/Phaellow
+                     + Add Microsoft.Microsoft3DViewer to inactive apps. Thanks to /u/Phaellow
+               1.1.1 + Add missing entries Microsoft.OneConnect, Microsoft.WindowsReadingList. Thanks to /u/1nfestissumam
+                     + Add Microsoft.MicrosoftStickyNotes to disabled list (user request)
+                     - Move Microsoft.Windows.CloudExperienceHost and Windows.ContactSupport to disabled list due to erroring out
+                     - Remove entry 9E2F88E3.Twitter (moved to Metro 3rd party list)
+               1.1.0 * Update script to use cleaner removal function. Thanks to /u/madbomb122
+               1.0.3 ! Fix typo in $PackagesToRemove array. Thanks to JegElsker
+               1.0.2 - Remove Calendar and Mail app from active target list. Thanks to /u/Reynbou
+               1.0.1 + Add script version and date variables to support automatic updates at Tron runtime
 #>
 $ErrorActionPreference = "SilentlyContinue"
 
@@ -14,9 +30,8 @@ $ErrorActionPreference = "SilentlyContinue"
 ########
 # PREP #
 ########
-$METRO_3RD_PARTY_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_VERSION = "1.2.5"
-$METRO_3RD_PARTY_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_DATE = "2018-07-18"
-
+$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_VERSION = "1.2.2"
+$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_DATE = "2018-07-18"
 
 # Needed for Removal
 $AppxPackages = Get-AppxProvisionedPackage -online | select-object PackageName,Displayname
@@ -28,24 +43,24 @@ Function Remove-App([String]$AppName){
 	If($AppxPackages.DisplayName -match $AppName -or $ProPackageList.Name -match $AppName ) {
 		$PackageFullName = ($ProPackageList | where {$_.Name -like $AppName}).PackageFullName
 		$ProPackageFullName = ($AppxPackages | where {$_.Displayname -like $AppName}).PackageName
-	
+
 		If($PackageFullName -is [array]){
 			For($i=0 ;$i -lt $PackageFullName.Length ;$i++) {
-				$Script:AppxCount3rd++
-				$Job = "TronScript3rd$AppxCount3rd"
+				$Script:AppxCountMS++
+				$Job = "TronScriptMS$AppxCountMS"
 				$PackageF = $PackageFullName[$i]
 				$ProPackage = $ProPackageFullName[$i]
-				write-output "$AppxCount3rd - $PackageF"
-				Start-Job -Name $Job -ScriptBlock { 
+				write-output "$AppxCountMS - $PackageF"
+				Start-Job -Name $Job -ScriptBlock {
 					Remove-AppxPackage -Package $using:PackageF | Out-null
 					Remove-AppxProvisionedPackage -Online -PackageName $using:ProPackage | Out-null
 				} | Out-null
 			}
 		} Else {
-			$Script:AppxCount3rd++
-			$Job = "TronScript3rd$AppxCount3rd"
-			write-output "$AppxCount3rd - $PackageFullName"
-			Start-Job -Name $Job -ScriptBlock { 
+			$Script:AppxCountMS++
+			$Job = "TronScriptMS$AppxCountMS"
+			write-output "$AppxCountMS - $PackageFullName"
+			Start-Job -Name $Job -ScriptBlock {
 				Remove-AppxPackage -Package $using:PackageFullName | Out-null
 				Remove-AppxProvisionedPackage -Online -PackageName $using:ProPackageFullName | Out-null
 			} | Out-null
@@ -53,179 +68,81 @@ Function Remove-App([String]$AppName){
 	}
 }
 
-
 ###########
 # EXECUTE #
 ###########
 # Active identifiers
-Remove-App "06DAC6F6.StumbleUpon"
-Remove-App "09B6C2D8.TheTreasuresofMontezuma3"
-Remove-App "134D4F5B.Box*"
-Remove-App "1430GreenfieldTechnologie.PuzzleTouch*"
-Remove-App "26720RandomSaladGamesLLC.HeartsDeluxe*"
-Remove-App "26720RandomSaladGamesLLC.SimpleSolitaire*"
-Remove-App "29982CsabaHarmath.UnCompress*"
-Remove-App "2FE3CB00.PicsArt-PhotoStudio*"
-Remove-App "37442SublimeCo.AlarmClockForYou"
-Remove-App "46928bounde.EclipseManager*"
-Remove-App "4AE8B7C2.Booking.comPartnerEdition*"
-Remove-App "5269FriedChicken.YouTubeVideosDownloader*"
-Remove-App "64885BlueEdge.OneCalendar*"
-Remove-App "6Wunderkinder.Wunderlist"
-Remove-App "7906AAC0.TOSHIBACanadaPartners*"
-Remove-App "7906AAC0.ToshibaCanadaWarrantyService*"
-Remove-App "7EE7776C.LinkedInforWindows"
-Remove-App "7digitalLtd.7digitalMusicStore*"
-Remove-App "828B5831.HiddenCityMysteryofShadows"
-Remove-App "89006A2E.AutodeskSketchBook*"
-Remove-App "95FE1D22.VUDUMoviesandTV"
-Remove-App "9E2F88E3.Twitter"
-Remove-App "A278AB0D.DisneyMagicKingdoms"
-Remove-App "A278AB0D.DragonManiaLegends*"
-Remove-App "A278AB0D.MarchofEmpires"
-Remove-App "A34E4AAB.YogaChef*"
-Remove-App "AD2F1837.DiscoverHPTouchpointManager"
-Remove-App "AD2F1837.HPJumpStart"
-Remove-App "AD2F1837.HPPowerManager"
-Remove-App "AD2F1837.HPWelcome"
-Remove-App "AD2F1837.SmartfriendbyHPCare"
-Remove-App "ASUSCloudCorporation.MobileFileExplorer*"
-Remove-App "AccuWeather.AccuWeatherforWindows8*"
-Remove-App "AcerIncorporated*"
-Remove-App "AcerIncorporated.AcerExplorer"
-Remove-App "ActiproSoftwareLLC*"
-Remove-App "ActiproSoftwareLLC.562882FEEB491"
-Remove-App "AdobeSystemsIncorporated.AdobePhotoshopExpress*"
-Remove-App "AdobeSystemsIncorporated.AdobeRevel*"
-Remove-App "Amazon.com.Amazon*"
-Remove-App "AppUp.IntelAppUpCatalogueAppWorldwideEdition*"
-Remove-App "B9ECED6F.ASUSGIFTBOX*"
-Remove-App "BD9B8345.AlbumbySony*"
-Remove-App "BD9B8345.MusicbySony*"
-Remove-App "BD9B8345.Socialife*"
-Remove-App "BD9B8345.VAIOCare*"
-Remove-App "BD9B8345.VAIOMessageCenter*"
-Remove-App "C27EB4BA.DropboxOEM"
-Remove-App "ChaChaSearch.ChaChaPushNotification*"
-Remove-App "ClearChannelRadioDigital.iHeartRadio*"
-Remove-App "CrackleInc.Crackle*"
-Remove-App "CyberLinkCorp.ac.AcerCrystalEye*"
-Remove-App "CyberLinkCorp.ac.SocialJogger*"
-Remove-App "CyberLinkCorp.hs.YouCamforHP*"
-Remove-App "CyberLinkCorp.id.PowerDVDforLenovoIdea*"
-Remove-App "D52A8D61.FarmVille2CountryEscape*"
-Remove-App "D5EA27B7.Duolingo-LearnLanguagesforFree*"
-Remove-App "DB6EA5DB.CyberLinkMediaSuiteEssentials*"
-Remove-App "DailymotionSA.Dailymotion*"
-Remove-App "DellInc.DellCustomerConnect"
-Remove-App "DellInc.DellHelpSupport"
-Remove-App "DellInc.DellPowerManager"
-Remove-App "DellInc.DellProductRegistration"
-Remove-App "DellInc.DellShop"
-Remove-App "DellInc.DellSupportAssistforPCs"
-Remove-App "DolbyLaboratories.DolbyAccess*"
-Remove-App "DolbyLaboratories.DolbyAtmosSoundSystem"
-Remove-App "Drawboard.DrawboardPDF*"
-Remove-App "DriverToaster*"
-Remove-App "E046963F.LenovoCompanion*"
-Remove-App "E046963F.LenovoSupport*"
-Remove-App "E0469640.CameraMan*"
-Remove-App "E0469640.DeviceCollaboration*"
-Remove-App "E0469640.LenovoRecommends*"
-Remove-App "E0469640.YogaCameraMan*"
-Remove-App "E0469640.YogaPhoneCompanion*"
-Remove-App "E0469640.YogaPicks*"
-Remove-App "ESPNInc.WatchESPN*"
-Remove-App "EncyclopaediaBritannica.EncyclopaediaBritannica*"
-Remove-App "Evernote.Evernote"
-Remove-App "Evernote.Skitch*"
-Remove-App "F5080380.ASUSPowerDirector*"
-Remove-App "Facebook.317180B0BB486"
-Remove-App "Facebook.Facebook"
-Remove-App "Facebook.InstagramBeta*"
-Remove-App "FilmOnLiveTVFree.FilmOnLiveTVFree*"
-Remove-App "Fingersoft.HillClimbRacing"
-Remove-App "FingertappsInstruments*"
-Remove-App "FingertappsOrganizer*"
-Remove-App "Flipboard.Flipboard*"
-Remove-App "FreshPaint*"
-Remove-App "GAMELOFTSA.Asphalt8Airborne*"
-Remove-App "GAMELOFTSA.SharkDash*"
-Remove-App "GameGeneticsApps.FreeOnlineGamesforLenovo*"
-Remove-App "GettingStartedwithWindows8*"
-Remove-App "GoogleInc.GoogleSearch"
-Remove-App "HPConnectedMusic*"
-Remove-App "HPConnectedPhotopoweredbySnapfish*"
-Remove-App "HPRegistration*"
-Remove-App "HuluLLC.HuluPlus*"
-Remove-App "JigsWar*"
-Remove-App "K-NFBReadingTechnologiesI.BookPlace*"
-Remove-App "KasperskyLab.KasperskyNow*"
-Remove-App "KeeperSecurityInc.Keeper"
-Remove-App "KindleforWindows8*"
-Remove-App "LenovoCorporation.LenovoID*"
-Remove-App "LenovoCorporation.LenovoSettings*"
-Remove-App "MAGIX.MusicMakerJam*"
-Remove-App "McAfee*"
-Remove-App "McAfeeInc.05.McAfeeSecurityAdvisorforASUS*"
-Remove-App "MobileFileExplorer*"
-Remove-App "MusicMakerJam*"
-Remove-App "NAMCOBANDAIGamesInc.PAC-MANChampionshipEditionDXfo*"
-Remove-App "NAVER.LINEwin8*"
-Remove-App "NBCUniversalMediaLLC.NBCSportsLiveExtra*"
-Remove-App "PandoraMediaInc.29680B314EFC2"
-Remove-App "PinballFx2*"
-Remove-App "Playtika.CaesarsSlotsFreeCasino*"
-Remove-App "Priceline"
-Remove-App "PricelinePartnerNetwork.Priceline.comTheBestDealso"
-Remove-App "PublicationsInternational.iCookbookSE*"
-Remove-App "RandomSaladGamesLLC.GinRummyProforHP*"
-Remove-App "SAMSUNGELECTRONICSCO.LTD.SamsungPrinterExperience"
-Remove-App "ShazamEntertainmentLtd.Shazam*"
-Remove-App "SonicWALL.MobileConnect"
-Remove-App "SpotifyAB.SpotifyMusic"
-Remove-App "SymantecCorporation.NortonStudio*"
-Remove-App "TelegraphMediaGroupLtd.TheTelegraphforLenovo*"
-Remove-App "TheNewYorkTimes.NYTCrossword*"
-Remove-App "ThumbmunkeysLtd.PhototasticCollage*"
-Remove-App "ToshibaAmericaInformation.ToshibaCentral*"
-Remove-App "TripAdvisorLLC.TripAdvisorHotelsFlightsRestaurants*"
-Remove-App "TuneIn.TuneInRadio*"
-Remove-App "UptoElevenDigitalSolution.mysms-Textanywhere*"
-Remove-App "Vimeo.Vimeo*"
-Remove-App "Weather.TheWeatherChannelforHP*"
-Remove-App "Weather.TheWeatherChannelforLenovo*"
-Remove-App "WildTangentGames*"
-Remove-App "WildTangentGames.63435CFB65F55"
-Remove-App "WinZipComputing.WinZipUniversal*"
-Remove-App "XINGAG.XING"
-Remove-App "XeroxCorp.PrintExperience"
-Remove-App "YouSendIt.HighTailForLenovo*"
-Remove-App "ZinioLLC.Zinio*"
-Remove-App "eBayInc.eBay*"
-Remove-App "esobiIncorporated.newsXpressoMetro*"
-Remove-App "fingertappsASUS.FingertappsInstrumentsrecommendedb*"
-Remove-App "fingertappsASUS.JigsWarrecommendedbyASUS*"
-Remove-App "fingertappsasus.FingertappsOrganizerrecommendedbyA*"
-Remove-App "flaregamesGmbH.RoyalRevolt2*"
-Remove-App "king.com.BubbleWitch3Saga"
-Remove-App "king.com.CandyCrushSaga"
-Remove-App "king.com.CandyCrushSodaSaga"
-Remove-App "sMedioforHP.sMedio360*"
-Remove-App "sMedioforToshiba.TOSHIBAMediaPlayerbysMedioTrueLin*"
-Remove-App "toolbar*"
-Remove-App "zuukaInc.iStoryTimeLibrary*"
+Remove-App "Microsoft.3DBuilder"
+Remove-App "Microsoft.Advertising.Xaml"                # Advertising framework
+Remove-App "Microsoft.BingFinance"                     # Money app - Financial news
+Remove-App "Microsoft.BingFoodAndDrink"                # Food and Drink app
+Remove-App "Microsoft.BingHealthAndFitness"            # Health and Fitness app
+Remove-App "Microsoft.BingMaps"
+Remove-App "Microsoft.BingNews"                        # Generic news app
+Remove-App "Microsoft.BingSports"                      # Sports app - Sports news
+Remove-App "Microsoft.BingTranslator"                  # Translator app - Bing Translate
+Remove-App "Microsoft.BingTravel"                      # Travel app
+Remove-App "Microsoft.CommsPhone"                      # Phone app
+Remove-App "Microsoft.ConnectivityStore"
+Remove-App "Microsoft.FreshPaint"                      # Canvas app
+Remove-App "Microsoft.GetHelp"                         # Get Help link
+Remove-App "Microsoft.Getstarted"                      # Get Started link
+Remove-App "Microsoft.Messaging"                       # Messaging app
+Remove-App "Microsoft.MicrosoftJackpot"                # Jackpot app
+Remove-App "Microsoft.MicrosoftJigsaw"                 # Jigsaw app
+Remove-App "Microsoft.MicrosoftMahjong"                # Advertising framework
+Remove-App "Microsoft.MicrosoftOfficeHub"
+Remove-App "Microsoft.MicrosoftPowerBIForWindows"      # Power BI app - Business analytics
+Remove-App "Microsoft.MicrosoftSudoku"
+Remove-App "Microsoft.MinecraftUWP"
+Remove-App "Microsoft.MovieMoments"                    # imported from stage_2_de-bloat.bat
+Remove-App "Microsoft.NetworkSpeedTest"
+Remove-App "Microsoft.Office.OneNote"                  # Onenote app
+Remove-App "Microsoft.Office.Sway"                     # Sway app
+Remove-App "Microsoft.OneConnect"                      # OneConnect app
+Remove-App "Microsoft.People"                          # People app
+Remove-App "Microsoft.SkypeApp"                        # Get Skype link
+Remove-App "Microsoft.SkypeWiFi"
+Remove-App "Microsoft.Studios.Wordament"               # imported from stage_2_de-bloat.bat
+Remove-App "Microsoft.WindowsFeedbackHub"              # Feedback app
+Remove-App "Microsoft.WindowsReadingList"
+Remove-App "Microsoft.Zune"                           # Zune collection of apps
+Remove-App "Windows.CBSPreview"
+Remove-App "Windows.ContactSupport"
 
 
 # Inactive identifers
-#Remove-App "Netflix*"
-#Remove-App "4DF9E0F8.Netflix"
+#Remove-App "Microsoft.Appconnector"                   # Not sure about this one
+#Remove-App "Microsoft.BingWeather"                    # Weather app
+#Remove-App "Microsoft.BioEnrollment"                  # not sure about this one
+#Remove-App "Microsoft.Microsoft3DViewer"              # 3D model viewer
+#Remove-App "Microsoft.MicrosoftSolitaireCollection"   # Solitaire collection
+#Remove-App "Microsoft.MicrosoftStickyNotes"           # Pulled from active list due to user requests
+#Remove-App "Microsoft.Windows.Photos"                 # Photos app
+#Remove-App "Microsoft.WindowsAlarms"                  # Alarms and Clock app
+#Remove-App "Microsoft.WindowsCalculator"              # Calculator app
+#Remove-App "Microsoft.WindowsCamera"                  # Camera app
+#Remove-App "Microsoft.WindowsMaps"                    # Maps app
+#Remove-App "Microsoft.WindowsSoundRecorder"           # Sound Recorder app
+#Remove-App "Microsoft.WindowsStore"                   # Windows Store
+#Remove-App "Microsoft.windowscommunicationsapps"      # Calendar and Mail app
+#Remove-App "Microsoft.MSPaint"                        # MS Paint (Paint 3D)
+#Remove-App "Microsoft.ZuneMusic"
+#Remove-App "Microsoft.ZuneVideo"
+#Remove-App "Microsoft.Xbox*"
+#Remove-App "Microsoft.Xbox.TCUI"
+#Remove-App "Microsoft.XboxApp"
+#Remove-App "Microsoft.XboxGameCallableUI"
+#Remove-App "Microsoft.XboxGameOverlay"
+#Remove-App "Microsoft.XboxGamingOverlay"
+#Remove-App "Microsoft.XboxIdentityProvider"
+#Remove-App "Microsoft.XboxSpeechToTextOverlay"
 
 ##########
 # Finish #
 ##########
 # DO NOT REMOVE OR CHANGE (needs to be at end of script)
-# Waits for Apps to be removed before Script Closes
+# Waits for Apps to be removed before script closes
 Write-Output 'Finishing app removal, please wait...'
-Wait-Job -Name "TronScript3rd*" | Out-null
-Remove-Job -Name "TronScript3rd*" | Out-null
+Wait-Job -Name "TronScriptMS*" | Out-Null
+Remove-Job -Name "TronScriptMS*" | Out-Null
