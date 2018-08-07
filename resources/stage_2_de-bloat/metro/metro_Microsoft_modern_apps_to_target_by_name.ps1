a2eabb25921a44cb05e11cb8f8d3598a0d456172
Vocatus Gate <vocatus.gate@gmail.com>
2018-08-07 21:20:00 +0000

    Update metro_Microsoft_modern_apps_to_target_by_name.ps1

--- resources/stage_2_de-bloat/metro/metro_Microsoft_modern_apps_to_target_by_name.ps1
+++ resources/stage_2_de-bloat/metro/metro_Microsoft_modern_apps_to_target_by_name.ps1
@@ -4,24 +4,8 @@ Purpose:       Script to remove many of the pre-loaded Microsoft Metro "modern a
 Requirements:  1. Administrator access
                2. Windows 8 and up
 Author:        vocatus on reddit.com/r/TronScript ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
-Version:       1.2.2 + Add additional user-submitted entries
-               1.2.1 * Fixed variable re-use, thanks to github:madbomb22
-               1.2.0 * Implement removal process improvements, thanks to github:madbomb22
-               1.1.8 + Add additional user-submitted entries
-               1.1.7 + Add Microsoft.GetHelp
-               1.1.6 ! Fix function evalution of * character. Thanks to u/madbomb122 and u/phant0md
-               1.1.5 + Add Zune entries, thanks to /u/ComputeGuy
-               1.1.4 + Add various entries, thanks to github:kronflux
-               1.1.3 + Add various entries
-               1.1.2 + Add Microsoft.WindowsFeedbackHub to active apps. Thanks to /u/Phaellow
-                     + Add Microsoft.Microsoft3DViewer to inactive apps. Thanks to /u/Phaellow
-               1.1.1 + Add missing entries Microsoft.OneConnect, Microsoft.WindowsReadingList. Thanks to /u/1nfestissumam
-                     + Add Microsoft.MicrosoftStickyNotes to disabled list (user request)
-                     - Move Microsoft.Windows.CloudExperienceHost and Windows.ContactSupport to disabled list due to erroring out
-                     - Remove entry 9E2F88E3.Twitter (moved to Metro 3rd party list)
-               1.1.0 * Update script to use cleaner removal function. Thanks to /u/madbomb122
-               1.0.3 ! Fix typo in $PackagesToRemove array. Thanks to JegElsker
-               1.0.2 - Remove Calendar and Mail app from active target list. Thanks to /u/Reynbou
+Version:       1.2.4 + Add additional user-submitted entries
+               <-- oudated comments removed -->
                1.0.1 + Add script version and date variables to support automatic updates at Tron runtime
 #>
 $ErrorActionPreference = "SilentlyContinue"
@@ -30,8 +14,8 @@ $ErrorActionPreference = "SilentlyContinue"
 ########
 # PREP #
 ########
-$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_VERSION = "1.2.2"
-$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_DATE = "2018-07-18"
+$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_VERSION = "1.2.4"
+$METRO_MICROSOFT_MODERN_APPS_TO_TARGET_BY_NAME_SCRIPT_DATE = "2018-08-07"
 
 # Needed for Removal
 $AppxPackages = Get-AppxProvisionedPackage -online | select-object PackageName,Displayname
@@ -81,6 +65,7 @@ Remove-App "Microsoft.BingNews"                        # Generic news app
 Remove-App "Microsoft.BingSports"                      # Sports app - Sports news
 Remove-App "Microsoft.BingTranslator"                  # Translator app - Bing Translate
 Remove-App "Microsoft.BingTravel"                      # Travel app
+Remove-App "Microsoft.BioEnrollment"
 Remove-App "Microsoft.CommsPhone"                      # Phone app
 Remove-App "Microsoft.ConnectivityStore"
 Remove-App "Microsoft.FreshPaint"                      # Canvas app
@@ -96,7 +81,6 @@ Remove-App "Microsoft.MicrosoftSudoku"
 Remove-App "Microsoft.MinecraftUWP"
 Remove-App "Microsoft.MovieMoments"                    # imported from stage_2_de-bloat.bat
 Remove-App "Microsoft.NetworkSpeedTest"
-Remove-App "Microsoft.Office.OneNote"                  # Onenote app
 Remove-App "Microsoft.Office.Sway"                     # Sway app
 Remove-App "Microsoft.OneConnect"                      # OneConnect app
 Remove-App "Microsoft.People"                          # People app
@@ -113,10 +97,11 @@ Remove-App "Windows.ContactSupport"
 # Inactive identifers
 #Remove-App "Microsoft.Appconnector"                   # Not sure about this one
 #Remove-App "Microsoft.BingWeather"                    # Weather app
-#Remove-App "Microsoft.BioEnrollment"                  # not sure about this one
+#Remove-App "Microsoft.MSPaint"                        # MS Paint (Paint 3D)
 #Remove-App "Microsoft.Microsoft3DViewer"              # 3D model viewer
 #Remove-App "Microsoft.MicrosoftSolitaireCollection"   # Solitaire collection
 #Remove-App "Microsoft.MicrosoftStickyNotes"           # Pulled from active list due to user requests
+#Remove-App "Microsoft.Office.OneNote"                  # Onenote app
 #Remove-App "Microsoft.Windows.Photos"                 # Photos app
 #Remove-App "Microsoft.WindowsAlarms"                  # Alarms and Clock app
 #Remove-App "Microsoft.WindowsCalculator"              # Calculator app
@@ -124,10 +109,6 @@ Remove-App "Windows.ContactSupport"
 #Remove-App "Microsoft.WindowsMaps"                    # Maps app
 #Remove-App "Microsoft.WindowsSoundRecorder"           # Sound Recorder app
 #Remove-App "Microsoft.WindowsStore"                   # Windows Store
-#Remove-App "Microsoft.windowscommunicationsapps"      # Calendar and Mail app
-#Remove-App "Microsoft.MSPaint"                        # MS Paint (Paint 3D)
-#Remove-App "Microsoft.ZuneMusic"
-#Remove-App "Microsoft.ZuneVideo"
 #Remove-App "Microsoft.Xbox*"
 #Remove-App "Microsoft.Xbox.TCUI"
 #Remove-App "Microsoft.XboxApp"
@@ -136,6 +117,9 @@ Remove-App "Windows.ContactSupport"
 #Remove-App "Microsoft.XboxGamingOverlay"
 #Remove-App "Microsoft.XboxIdentityProvider"
 #Remove-App "Microsoft.XboxSpeechToTextOverlay"
+#Remove-App "Microsoft.ZuneMusic"
+#Remove-App "Microsoft.ZuneVideo"
+#Remove-App "Microsoft.windowscommunicationsapps"      # Calendar and Mail app
 
 ##########
 # Finish #
