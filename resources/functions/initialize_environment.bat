b4eb8e2bdc4293e354b8c021ac1f58d850ad391c
Vocatus Gate <vocatus.gate@gmail.com>
2019-06-02 11:16:05 -0600

    Update initialize_environment.bat

--- resources/functions/initialize_environment.bat
+++ resources/functions/initialize_environment.bat
@@ -6,8 +6,8 @@
 
 
 :: Tron Project version and date. These two variables determine the overall project version and date
-set TRON_VERSION=10.8.0
-set TRON_DATE=2019-03-09
+set TRON_VERSION=10.8.1
+set TRON_DATE=2019-05-16
 
 :: Set window title
 title Tron v%TRON_VERSION% (%TRON_DATE%)
