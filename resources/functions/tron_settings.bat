410a9864ac1f9fcb457591e06bbfdba54d931e9d
Vocatus Gate <vocatus.gate@gmail.com>
2024-03-09 11:03:05 -0700

    Update tron_settings.bat

--- resources/functions/tron_settings.bat
+++ resources/functions/tron_settings.bat
@@ -2,7 +2,9 @@
 ::                Sub-stage scripts also call this file if they're launched directly.
 :: Requirements:  n/a
 :: Author:        vocatus on reddit.com/r/TronScript ( vocatus.gate at gmail ) // PGP key: 0x07d1490f82a211a2
-:: Version:       1.0.5 - Remove references to Adobe Flash
+:: Version:       1.0.7 - Remove all code related to Sophos due to it being deprecated
+::                1.0.6 + Add SKIP_ADWCLEANER_SCAN (-sac) switch. Thanks to u/fr0stedfl4ke
+::                1.0.5 - Remove references to Adobe Flash
 ::                1.0.4 + Add PRESERVE_MALWAREBYTES (-rmb) switch to have Tron automatically remove Malwarebytes at the end of the run
 ::                      + Add SKIP_COOKIE_CLEANUP (-scc) switch to have Tron preserve ALL cookies. Thanks to tbr:sebastian
 ::                1.0.3 + Add SKIP_ONEDRIVE_REMOVAL (-sor) switch. Thanks to github:ptrkhh
@@ -11,8 +13,8 @@
 ::                1.0.0 . Initial write; forked out of v9.9.0 of tron.bat
 
 :: Script version
-set TRON_SETTINGS_SCRIPT_VERSION=1.0.5
-set TRON_SETTINGS_SCRIPT_DATE=2021-01-15
+set TRON_SETTINGS_SCRIPT_VERSION=1.0.7
+set TRON_SETTINGS_SCRIPT_DATE=2024-03-09
 
 
 :::::::::::::::
@@ -63,7 +65,8 @@ set SUMMARY_LOGS=%LOGPATH%\summary_logs
 :: PRESERVE_POWER_SCHEME  (-p)   = Preserve active power scheme. Default is to reset power scheme to Windows defaults at the end of Tron
 :: PRESERVE_MALWAREBYTES  (-pmb) = Preserve Malwarebytes (don't uninstall it) after Tron is complete
 :: AUTO_REBOOT_DELAY      (-r)   = Post-run delay (in seconds) before rebooting. Set to 0 to disable auto-reboot
-:: SKIP_ANTIVIRUS_SCANS   (-sa)  = Skip ALL antivirus scans (KVRT, MBAM, SAV). Use per-scanner switches to individually toggle usage
+:: SKIP_ANTIVIRUS_SCANS   (-sa)  = Skip ALL antivirus scans (ADW, KVRT, MBAM, SAV). Use per-scanner switches to individually toggle usage
+:: SKIP_ADWCLEANER_SCAN   (-sac) = Set to yes to skip AdwCleaner scan
 :: SKIP_APP_PATCHES       (-sap) = Set to yes to skip application patches (don't patch 7-Zip)
 :: SKIP_COOKIE_CLEANUP    (-scc) = Set to yes to preserve ALL cookies (not recommended, Tron auto-preserves most common login cookies)
 :: SKIP_CUSTOM_SCRIPTS    (-scs) = Set to yes to forcibly skip Stage 8: Custom Scripts regardless whether or not .bat files exist in the directory
@@ -76,12 +79,11 @@ set SUMMARY_LOGS=%LOGPATH%\summary_logs
 :: SKIP_MBAM_INSTALL      (-sm)  = Set to yes to skip Malwarebytes Anti-Malware installation
 :: SKIP_ONEDRIVE_REMOVAL  (-sor) = Set to yes to skip OneDrive removal regardless whether it's in use or not
 :: SKIP_PAGEFILE_RESET    (-spr) = Skip page file settings reset (don't set to "Let Windows manage the page file")
-:: SKIP_SOPHOS_SCAN       (-ss)  = Set to yes to skip Sophos Anti-Virus scan
 :: SKIP_TELEMETRY_REMOVAL (-str) = Set to yes to skip Telemetry Removal (just turn telemetry off instead of removing it)
 :: SKIP_WINDOWS_UPDATES   (-swu) = Set to yes to skip Windows Updates entirely (ignore both WSUS Offline and online methods)
 :: SKIP_WSUS_OFFLINE      (-swo) = Set to yes to skip user-supplied WSUS Offline updates (if they exist; online updates still attempted)
 :: UPLOAD_DEBUG_LOGS      (-udl) = Upload debug logs. Send tron.log and the system GUID dump to the Tron developer. Please use this if possible, logs are extremely helpful in Tron development
-:: VERBOSE                (-v)   = When possible, show as much output as possible from each program Tron calls (e.g. Sophos, KVRT, etc). NOTE: This is often much slower
+:: VERBOSE                (-v)   = When possible, show as much output as possible from each program Tron calls (e.g. ADW, KVRT, etc). NOTE: This is often much slower
 :: SELF_DESTRUCT          (-x)   = Set to yes to have Tron automatically delete itself after running. Leaves logs intact
 set AUTORUN=no
 set AUTORUN_IN_SAFE_MODE=no
@@ -96,6 +98,7 @@ set PRESERVE_POWER_SCHEME=no
 set PRESERVE_MALWAREBYTES=no
 set AUTO_REBOOT_DELAY=0
 set SKIP_ANTIVIRUS_SCANS=no
+set SKIP_ADWCLEANER_SCAN=no
 set SKIP_APP_PATCHES=no
 set SKIP_COOKIE_CLEANUP=no
 set SKIP_CUSTOM_SCRIPTS=no
@@ -104,12 +107,10 @@ set SKIP_DEBLOAT=no
 set SKIP_DISM_CLEANUP=no
 set SKIP_DEBLOAT_UPDATE=no
 set SKIP_EVENT_LOG_CLEAR=no
-set SKIP_ADWCLEANER_SCAN=no
 set SKIP_KASPERSKY_SCAN=no
 set SKIP_MBAM_INSTALL=no
 set SKIP_ONEDRIVE_REMOVAL=no
 set SKIP_PAGEFILE_RESET=no
-set SKIP_SOPHOS_SCAN=no
 set SKIP_TELEMETRY_REMOVAL=no
 set SKIP_WINDOWS_UPDATES=no
 set SKIP_WSUS_OFFLINE=no
